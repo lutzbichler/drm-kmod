333c47be6691a7ce76801f99bd4c995030d14454
Jack Xiao <Jack.Xiao@amd.com>
2021-04-14 16:22:43 +0800

    drm/amdgpu: allocate doorbell index for mes kiq
    Allocate a doorbell index for mes kiq queue.
    
    Signed-off-by: Jack Xiao <Jack.Xiao@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/mes_v10_1.c
+++ drivers/gpu/drm/amd/amdgpu/mes_v10_1.c
@@ -813,7 +813,7 @@ static int mes_v10_1_ring_init(struct amdgpu_device *adev)
 
 	ring->ring_obj = NULL;
 	ring->use_doorbell = true;
-	ring->doorbell_index = adev->doorbell_index.mes_ring << 1;
+	ring->doorbell_index = adev->doorbell_index.mes_ring0 << 1;
 	ring->eop_gpu_addr = adev->mes.eop_gpu_addr;
 	ring->no_scheduler = true;
 	sprintf(ring->name, "mes_%d.%d.%d", ring->me, ring->pipe, ring->queue);
