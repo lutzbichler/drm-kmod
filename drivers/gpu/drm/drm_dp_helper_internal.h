f44375c18e1ef34d6f1faf1b1bc11d88fd98b2e6
Thomas Zimmermann <tzimmermann@suse.de>
2022-01-14 12:45:32 +0100

    drm/dp: Move DP declarations into separate header file
    Split the DP declarations from other helpers before moving the
    DP functions into a separate module.
    
    v2:
            * forward-declare struct drm_dp_aux (Jani)
            * add include guards in drm_dp_helper_internal.h
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Acked-by: Daniel Vetter <daniel@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220114114535.29157-3-tzimmermann@suse.de

---
+++ drivers/gpu/drm/drm_dp_helper_internal.h
@@ -0,0 +1,32 @@
+/* Public domain */
+#ifndef _DRM_DP_HELPER_INTERNAL_H_
+#define _DRM_DP_HELPER_INTERNAL_H_
+
+/* drm_dp_aux_dev.c */
+#ifdef CONFIG_DRM_DP_AUX_CHARDEV
+int drm_dp_aux_dev_init(void);
+void drm_dp_aux_dev_exit(void);
+int drm_dp_aux_register_devnode(struct drm_dp_aux *aux);
+void drm_dp_aux_unregister_devnode(struct drm_dp_aux *aux);
+#else
+static inline int drm_dp_aux_dev_init(void)
+{
+	return 0;
+}
+
+static inline void drm_dp_aux_dev_exit(void)
+{
+}
+
+static inline int drm_dp_aux_register_devnode(struct drm_dp_aux *aux)
+{
+	return 0;
+}
+
+static inline void drm_dp_aux_unregister_devnode(struct drm_dp_aux *aux)
+{
+}
+#endif
+
+#endif /* _DRM_DP_HELPER_INTERNAL_H_ */
+
