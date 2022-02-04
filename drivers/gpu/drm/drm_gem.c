8f1b4ea3a83d711d32191ab3c8c819e696319c52
Lucas De Marchi <lucas.demarchi@intel.com>
2022-02-04 09:05:41 -0800

    dma-buf-map: Rename to iosys-map MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    Rename struct dma_buf_map to struct iosys_map and corresponding APIs.
    Over time dma-buf-map grew up to more functionality than the one used by
    dma-buf: in fact it's just a shim layer to abstract system memory, that
    can be accessed via regular load and store, from IO memory that needs to
    be acessed via arch helpers.
    
    The idea is to extend this API so it can fulfill other needs, internal
    to a single driver. Example: in the i915 driver it's desired to share
    the implementation for integrated graphics, which uses mostly system
    memory, with discrete graphics, which may need to access IO memory.
    
    The conversion was mostly done with the following semantic patch:
    
            @r1@
            @@
            - struct dma_buf_map
            + struct iosys_map
    
            @r2@
            @@
            (
            - DMA_BUF_MAP_INIT_VADDR
            + IOSYS_MAP_INIT_VADDR
            |
            - dma_buf_map_set_vaddr
            + iosys_map_set_vaddr
            |
            - dma_buf_map_set_vaddr_iomem
            + iosys_map_set_vaddr_iomem
            |
            - dma_buf_map_is_equal
            + iosys_map_is_equal
            |
            - dma_buf_map_is_null
            + iosys_map_is_null
            |
            - dma_buf_map_is_set
            + iosys_map_is_set
            |
            - dma_buf_map_clear
            + iosys_map_clear
            |
            - dma_buf_map_memcpy_to
            + iosys_map_memcpy_to
            |
            - dma_buf_map_incr
            + iosys_map_incr
            )
    
            @@
            @@
            - #include <linux/dma-buf-map.h>
            + #include <linux/iosys-map.h>
    
    Then some files had their includes adjusted and some comments were
    update to remove mentions to dma-buf-map.
    
    Since this is not specific to dma-buf anymore, move the documentation to
    the "Bus-Independent Device Accesses" section.
    
    v2:
      - Squash patches
    
    v3:
      - Fix wrong removal of dma-buf.h from MAINTAINERS
      - Move documentation from dma-buf.rst to device-io.rst
    
    v4:
      - Change documentation title and level
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Acked-by: Sumit Semwal <sumit.semwal@linaro.org>
    Acked-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220204170541.829227-1-lucas.demarchi@intel.com

--- drivers/gpu/drm/drm_gem.c
+++ drivers/gpu/drm/drm_gem.c
@@ -29,6 +29,7 @@
 #include <linux/dma-buf.h>
 #include <linux/file.h>
 #include <linux/fs.h>
+#include <linux/iosys-map.h>
 #include <linux/mem_encrypt.h>
 #include <linux/mm.h>
 #include <linux/mman.h>
@@ -42,7 +43,6 @@
 #include <linux/slab.h>
 #include <linux/types.h>
 #include <linux/uaccess.h>
-
 #include <drm/drm.h>
 #include <drm/drm_device.h>
 #include <drm/drm_drv.h>
@@ -1178,7 +1178,7 @@ void drm_gem_unpin(struct drm_gem_object *obj)
 		obj->funcs->unpin(obj);
 }
 
-int drm_gem_vmap(struct drm_gem_object *obj, struct dma_buf_map *map)
+int drm_gem_vmap(struct drm_gem_object *obj, struct iosys_map *map)
 {
 	int ret;
 
@@ -1188,23 +1188,23 @@ int drm_gem_vmap(struct drm_gem_object *obj, struct dma_buf_map *map)
 	ret = obj->funcs->vmap(obj, map);
 	if (ret)
 		return ret;
-	else if (dma_buf_map_is_null(map))
+	else if (iosys_map_is_null(map))
 		return -ENOMEM;
 
 	return 0;
 }
 EXPORT_SYMBOL(drm_gem_vmap);
 
-void drm_gem_vunmap(struct drm_gem_object *obj, struct dma_buf_map *map)
+void drm_gem_vunmap(struct drm_gem_object *obj, struct iosys_map *map)
 {
-	if (dma_buf_map_is_null(map))
+	if (iosys_map_is_null(map))
 		return;
 
 	if (obj->funcs->vunmap)
 		obj->funcs->vunmap(obj, map);
 
 	/* Always set the mapping to NULL. Callers may rely on this. */
-	dma_buf_map_clear(map);
+	iosys_map_clear(map);
 }
 EXPORT_SYMBOL(drm_gem_vunmap);
 
