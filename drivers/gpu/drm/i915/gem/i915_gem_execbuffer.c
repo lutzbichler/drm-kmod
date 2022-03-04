a568a3ad15f1c155f825884361e8b3181ff30e8e
Thomas Hellström <thomas.hellstrom@linux.intel.com>
2022-03-04 09:26:39 +0100

    drm/i915: Remove the vm open count
    vms are not getting properly closed. Rather than fixing that,
    Remove the vm open count and instead rely on the vm refcount.
    
    The vm open count existed solely to break the strong references the
    vmas had on the vms. Now instead make those references weak and
    ensure vmas are destroyed when the vm is destroyed.
    
    Unfortunately if the vm destructor and the object destructor both
    wants to destroy a vma, that may lead to a race in that the vm
    destructor just unbinds the vma and leaves the actual vma destruction
    to the object destructor. However in order for the object destructor
    to ensure the vma is unbound it needs to grab the vm mutex. In order
    to keep the vm mutex alive until the object destructor is done with
    it, somewhat hackishly grab a vm_resv refcount that is released late
    in the vma destruction process, when the vm mutex is no longer needed.
    
    v2: Address review-comments from Niranjana
    - Clarify that the struct i915_address_space::skip_pte_rewrite is a hack
      and should ideally be replaced in an upcoming patch.
    - Remove an unneeded continue in clear_vm_list and update comment.
    
    v3:
    - Documentation update
    - Commit message formatting
    
    Co-developed-by: Niranjana Vishwanathapura <niranjana.vishwanathapura@intel.com>
    Signed-off-by: Niranjana Vishwanathapura <niranjana.vishwanathapura@intel.com>
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Reviewed-by: Niranjana Vishwanathapura <niranjana.vishwanathapura@intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220304082641.308069-2-thomas.hellstrom@linux.intel.com

--- drivers/gpu/drm/i915/gem/i915_gem_execbuffer.c
+++ drivers/gpu/drm/i915/gem/i915_gem_execbuffer.c
@@ -2707,6 +2707,11 @@ eb_select_engine(struct i915_execbuffer *eb)
 	if (err)
 		goto err;
 
+	if (!i915_vm_tryget(ce->vm)) {
+		err = -ENOENT;
+		goto err;
+	}
+
 	eb->context = ce;
 	eb->gt = ce->engine->gt;
 
@@ -2730,6 +2735,7 @@ eb_put_engine(struct i915_execbuffer *eb)
 {
 	struct intel_context *child;
 
+	i915_vm_put(eb->context->vm);
 	intel_gt_pm_put(eb->gt);
 	for_each_child(eb->context, child)
 		intel_context_put(child);
