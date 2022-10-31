0a44439f0899115a3d5727a1a3a984f1a22ad98c
Oded Gabbay <ogabbay@kernel.org>
2022-10-31 15:31:07 +0200

    drm: initialize accel framework
    Now that we have the accel framework code ready, let's call the
    accel functions from all the appropriate places. These places are the
    drm module init/exit functions, and all the drm_minor handling
    functions.
    
    Signed-off-by: Oded Gabbay <ogabbay@kernel.org>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Jeffrey Hugo <quic_jhugo@quicinc.com>
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Acked-by: Thomas Zimmermann <tzimmermann@suse.de>
    Acked-by: Jacek Lawrynowicz <jacek.lawrynowicz@linux.intel.com>
    Tested-by: Jacek Lawrynowicz <jacek.lawrynowicz@linux.intel.com>
    Reviewed-by: Melissa Wen <mwen@igalia.com>

---
+++ include/drm/drm_accel.h
@@ -0,0 +1,47 @@
+/* Public domain */
+
+#ifndef DRM_ACCEL_H_
+#define DRM_ACCEL_H_
+
+struct drm_minor;
+
+static inline int
+accel_core_init(void)
+{
+
+    return (0);
+}
+
+static inline void
+accel_core_exit(void)
+{
+
+}
+
+static inline int
+accel_minor_alloc(void)
+{
+
+    return (-ENOTSUPP);
+}
+
+static inline void
+accel_minor_remove(int index __unused)
+{
+
+} 
+
+static inline void
+accel_minor_replace(struct drm_minor *minor __unused, int index __unused)
+{
+
+}
+
+static inline void
+accel_debugfs_init(struct drm_minor *minor __unused, int index __unused)
+{
+
+}
+
+#endif /* DRM_ACCEL_H_ */
+
