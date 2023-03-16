815ae15ecb6c98a65f54197a39cf174bf00ada3b
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2023-03-16 21:49:41 +0100

    vt(4) integration: Uncomment everything in `drm_fb_helper.c`
    ... except the code related specifically to sysrq and framebuffer
    deferred I/O.
    
    [Why]
    In Linux 5.17, the amdgpu driver relies entirely on `drm_fb_helper` to
    handle its framebuffer. Indeed, the framebuffer-specific code is
    removed.
    
    Also in Linux 5.17, something changed in the i915 driver. I don't know
    exactly what, but the memory region passed to vt(4) and `vt_fb` isn't
    the one displayed on the screen. Therefore, all writes made by `vt_fb`
    leads to no changes on the screen and the computer looks frozen (that's
    not the case). However, when I vt-switch to another tty, the screen is
    refreshed to show the content of the tty active *before* the switch.
    Switching tty is the only time `vt_fb` calls into DRM. There is enough
    evidence that the i915 driver now assumes that the framebuffer is
    accessed like `drm_fb_helper` does it, i.e. like a Linux fbdev device.
    
    Unfortunately, our vt(4) integration is way way simpler than than Linux'
    fbdev.
    
    The goal of this commit and following ones is to change that vt(4)
    integration layer to behave like fbdev.
    
    [How]
    The first step is uncomment everything in `drm_fb_helper` to make sure
    that the API itself is ok.
    
    `drm_fb_helper` depends on lower-level functions, especially
    `fb_sys_{read,write}` and `fb_cfb_{read,write}` which are responsible
    for all reads and writes to the allocated memory region for the
    framebuffer. In this commit, they are simple stubs. They will be
    implemented in a future commit.
    
    As mentionned at the top, two areas are left commentted out:
    * sysrq, a Linux specific mechanism to interact with the kernel at a
      very low level. There is something close in FreeBSD, but we don't need
      to worry about it in `drm_fb_helper.
    * framebuffer deferred I/O, a framebuffer feature to flush the memory
      region asynchronously. We can't really implement that on FreeBSD and I
      believe we don't need to.

--- linuxkpi/bsd/include/uapi/linux/fb.h
+++ linuxkpi/bsd/include/uapi/linux/fb.h
@@ -27,11 +27,133 @@
 #ifndef _BSD_LKPI_UAPI_LINUX_FB_H_
 #define	_BSD_LKPI_UAPI_LINUX_FB_H_
 
+#define	FB_ACCEL_NONE	0
+
+#define	FB_ACCELF_TEXT	1
+
+#define	FB_ACTIVATE_NOW	1
+
+#define	FB_BLANK_UNBLANK       0
+#define	FB_BLANK_NORMAL	       1
+#define	FB_BLANK_HSYNC_SUSPEND 2
+#define	FB_BLANK_VSYNC_SUSPEND 3
+#define	FB_BLANK_POWERDOWN     4
+
 #define	FB_ROTATE_UR	0
 #define	FB_ROTATE_CW	1
 #define	FB_ROTATE_UD	2
 #define	FB_ROTATE_CCW	3
 
+#define	FB_SCREENINFO_ID 16
+
+#define	FB_TYPE_PACKED_PIXELS 1
+
+#define	FB_VISUAL_PSEUDOCOLOR 1
+#define	FB_VISUAL_TRUECOLOR   2
+
 #define	FBIO_WAITFORVSYNC	_IOW('F', 0x20, __u32)
+
+#define	KHZ2PICOS(x) 	(1000000000ul * (x))
+
+#define	ROP_COPY 1
+
+struct fb_bitfield {
+	uint32_t offset;
+	uint32_t length;
+	bool	 msb_right;
+};
+
+struct fb_cmap {
+	uint32_t start;
+	uint32_t len;
+
+	uint16_t *red;
+	uint16_t *green;
+	uint16_t *blue;
+};
+
+struct fb_copyarea {
+	uint32_t dx;
+	uint32_t dy;
+
+	uint32_t width;
+	uint32_t height;
+};
+
+struct fb_cursor {
+};
+
+struct fb_fillrect {
+	uint32_t dx;
+	uint32_t dy;
+
+	uint32_t width;
+	uint32_t height;
+
+	uint32_t color;
+	int		 rop;
+};
+
+struct fb_fix_screeninfo {
+	int		   line_length;
+	int		   visual;
+
+	int		   type;
+	int		   type_aux;
+
+	int		   accel;
+
+	int		   xpanstep;
+	int		   ypanstep;
+	int		   ywrapstep;
+
+	vm_paddr_t mmio_start;
+	uint64_t   mmio_len;
+
+	vm_paddr_t smem_start;
+	uint64_t   smem_len;
+
+	char	   id[FB_SCREENINFO_ID];
+};
+
+struct fb_image {
+	uint32_t dx;
+	uint32_t dy;
+
+	uint32_t width;
+	uint32_t height;
+	uint32_t depth;
+
+	uint32_t fg_color;
+	uint32_t bg_color;
+
+	const char *data;
+};
+
+struct fb_var_screeninfo {
+	uint32_t width;
+	uint32_t height;
+
+	int		 xres;
+	int		 yres;
+	int		 xres_virtual;
+	int		 yres_virtual;
+
+	uint32_t xoffset;
+	uint32_t yoffset;
+
+	int		 bits_per_pixel;
+	int		 grayscale;
+
+	uint32_t pixclock;
+
+	int		 accel_flags;
+	int		 activate;
+
+	struct fb_bitfield red;
+	struct fb_bitfield green;
+	struct fb_bitfield blue;
+	struct fb_bitfield transp;
+};
 
 #endif	/* _BSD_LKPI_UAPI_FB_H_ */
