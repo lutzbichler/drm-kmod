4d7eefd5305bef259390a209883a38c8169d7aef
Lutz Bichler <Lutz.Bichler@gmail.com>
2024-03-27 19:23:28 +0100

    pr/xe: Initial import of drm/xe driver

---
+++ drivers/gpu/drm/xe/xe_trace.h
@@ -0,0 +1,25 @@
+/* Public domain */
+
+#ifndef _XE_TRACE_FREEBSD_H_
+#define _XE_TRACE_FREEBSD_H_
+
+#include <sys/param.h>
+#include <sys/ktr.h>
+
+static inline void
+trace_xe_vma_evict(struct xe_vma *vma)
+{
+}
+
+static inline void
+trace_xe_bo_move(struct xe_bo *bo, u32 new_mem_type, u32 old_mem_type, bool move_lacks_source)
+{
+}
+
+static inline void
+trace_xe_bo_cpu_fault(struct xe_bo *bo)
+{
+}
+
+#endif /* _XE_TRACE_FREEBSD_H_ */
+
