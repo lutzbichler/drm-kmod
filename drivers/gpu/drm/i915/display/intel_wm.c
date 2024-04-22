1c55ab19254980bb602d5581903f7cde94249415
Jani Nikula <jani.nikula@intel.com>
2024-04-22 15:10:10 +0300

    drm/print: drop include debugfs.h and include where needed
    Surprisingly many places depend on debugfs.h to be included via
    drm_print.h. Fix them.
    
    v3: Also fix armada, ite-it6505, imagination, msm, sti, vc4, and xe
    
    v2: Also fix ivpu and vmwgfx
    
    Reviewed-by: Andrzej Hajda <andrzej.hajda@intel.com>
    Acked-by: Maxime Ripard <mripard@kernel.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240410141434.157908-1-jani.nikula@intel.com
    Acked-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org> # drm/msm
    Acked-by: Matt Coster <matt.coster@imgtec.com> # drm/imagination
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Acked-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Acked-by: Robert Foss <rfoss@kernel.org> #drm/bridge
    Reviewed-by: Lyude Paul <lyude@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240422121011.4133236-1-jani.nikula@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_wm.c
+++ drivers/gpu/drm/i915/display/intel_wm.c
@@ -3,6 +3,10 @@
  * Copyright © 2023 Intel Corporation
  */
 
+#ifdef __FreeBSD__
+#include <linux/debugfs.h>
+#endif
+
 #include "i915_drv.h"
 #include "i9xx_wm.h"
 #include "intel_display_types.h"
