870b64acf24ce0ae279ac38a7772dd7ceb545df7
Jani Nikula <jani.nikula@intel.com>
2024-04-29 19:43:36 +0300

    drm: deprecate driver date
    The driver date serves no useful purpose, because it's hardly ever
    updated. The information is misleading at best.
    
    As described in Documentation/gpu/drm-internals.rst:
    
      The driver date, formatted as YYYYMMDD, is meant to identify the date
      of the latest modification to the driver. However, as most drivers
      fail to update it, its value is mostly useless. The DRM core prints it
      to the kernel log at initialization time and passes it to userspace
      through the DRM_IOCTL_VERSION ioctl.
    
    Stop printing the driver date at init, and start returning the empty
    string "" as driver date through the DRM_IOCTL_VERSION ioctl.
    
    The driver date initialization in drivers and the struct drm_driver date
    member can be removed in follow-up.
    
    Reviewed-by: Hamza Mahfooz <hamza.mahfooz@amd.com>
    Acked-by: Simon Ser <contact@emersion.fr>
    Reviewed-by: Javier Martinez Canillas <javierm@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240429164336.1406480-1-jani.nikula@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- include/drm/drm_drv.h
+++ include/drm/drm_drv.h
@@ -409,7 +409,7 @@ struct drm_driver {
 	char *name;
 	/** @desc: driver description */
 	char *desc;
-	/** @date: driver date */
+	/** @date: driver date, unused, to be removed */
 	char *date;
 
 	/**
