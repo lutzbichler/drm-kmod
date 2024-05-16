4facacd50f36d633f4d3d333c64148a23803f6f0
Michal Wajdeczko <michal.wajdeczko@intel.com>
2024-05-16 18:00:15 +0200

    drm/print: Kill ___drm_dbg()
    There is no point in maintaining a separate print function, while
    there is __drm_dev_dbg() function that can work with a NULL device.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240516160015.2260-1-michal.wajdeczko@intel.com
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- include/drm/drm_print.h
+++ include/drm/drm_print.h
@@ -594,8 +594,6 @@ void __drm_dev_dbg(struct _ddebug *desc, const struct device *dev,
  */
 
 #ifdef __linux__
-__printf(3, 4)
-void ___drm_dbg(struct _ddebug *desc, enum drm_debug_category category, const char *format, ...);
 __printf(1, 2)
 void __drm_err(const char *format, ...);
 #else
@@ -607,11 +605,11 @@ void __drm_err(const char *function_name, const char *format, ...);
 #endif
 
 #if !defined(CONFIG_DRM_USE_DYNAMIC_DEBUG)
-#define __drm_dbg(cat, fmt, ...)		___drm_dbg(NULL, cat, fmt, ##__VA_ARGS__)
+#define __drm_dbg(cat, fmt, ...)	__drm_dev_dbg(NULL, NULL, cat, fmt, ##__VA_ARGS__)
 #else
 #define __drm_dbg(cat, fmt, ...)					\
-	_dynamic_func_call_cls(cat, fmt, ___drm_dbg,			\
-			       cat, fmt, ##__VA_ARGS__)
+	_dynamic_func_call_cls(cat, fmt, __drm_dev_dbg,			\
+			       NULL, cat, fmt, ##__VA_ARGS__)
 #endif
 
 /* Macros to make printk easier */
