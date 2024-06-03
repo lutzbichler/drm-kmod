21fc3fb3bd3f590d52d27af2a05787d43d485c54
Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>
2024-06-03 14:25:50 +0300

    drm/i915: Rename bigjoiner master/slave to bigjoiner primary/secondary
    According to BSpec we now should call "master" pipes, "primary" pipes
    and "slave" pipes, should be "secondary" pipes.
    
    Signed-off-by: Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>
    [vsyrjala: Don't rename port sync stuff, catch a few more things]
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240603112551.6481-3-stanislav.lisovskiy@intel.com

--- drivers/gpu/drm/i915/display/intel_drrs.c
+++ drivers/gpu/drm/i915/display/intel_drrs.c
@@ -161,7 +161,7 @@ void intel_drrs_activate(const struct intel_crtc_state *crtc_state)
 	if (!crtc_state->hw.active)
 		return;
 
-	if (intel_crtc_is_joiner_slave(crtc_state))
+	if (intel_crtc_is_joiner_secondary(crtc_state))
 		return;
 
 	mutex_lock(&crtc->drrs.mutex);
@@ -193,7 +193,7 @@ void intel_drrs_deactivate(const struct intel_crtc_state *old_crtc_state)
 	if (!old_crtc_state->hw.active)
 		return;
 
-	if (intel_crtc_is_joiner_slave(old_crtc_state))
+	if (intel_crtc_is_joiner_secondary(old_crtc_state))
 		return;
 
 	mutex_lock(&crtc->drrs.mutex);
