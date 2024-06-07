17425c40dc12b5206cdd31d4a47127cf8cfb3d04
Radhakrishna Sripada <radhakrishna.sripada@intel.com>
2024-06-07 11:29:41 -0700

    drm/xe/trace: Print device_id in xe_trace_guc events MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    In multi-gpu environments it is important to know the device
    guc txn belongs to. The tracing information includes the device_id
    to indicate the device the event is associated with.
    
    v2: Use variable sized variant to display dev name(Gustavo)
    v3: Pass single argument to __assign_str to fix kunit error
    v4: Minor formatting tweaks
    
    Suggested-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Reviewed-by: Gustavo Sousa <gustavo.sousa@intel.com>
    Signed-off-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240607182943.3572524-5-radhakrishna.sripada@intel.com

--- drivers/gpu/drm/xe/xe_trace_guc.h
+++ drivers/gpu/drm/xe/xe_trace_guc.h
@@ -7,22 +7,26 @@
 #include <sys/ktr.h>
 
 static inline void
-trace_xe_guc_ctb_g2h(u32 head, u32 tail, u32 size, u32 space, u32 len)
+trace_xe_guc_ctb_g2h(struct xe_device *xe, u32 head, u32 tail, u32 size,
+        u32 space, u32 len)
 {
 }
 
 static inline void
-trace_xe_guc_ctb_h2g(u32 head, u32 tail, u32 size, u32 space, u32 len)
+trace_xe_guc_ctb_h2g(struct xe_device *xe, u32 head, u32 tail, u32 size,
+        u32 space, u32 len)
 {
 }
 
 static inline void
-trace_xe_guc_ct_g2h_flow_control(u32 head, u32 tail, u32 size, u32 space, u32 len)
+trace_xe_guc_ct_g2h_flow_control(struct xe_device *xe, u32 head, u32 tail,
+        u32 size, u32 space, u32 len)
 {
 }
 
 static inline void
-trace_xe_guc_ct_h2g_flow_control(u32 head, u32 tail, u32 size, u32 space, u32 len)
+trace_xe_guc_ct_h2g_flow_control(struct xe_device *xe, u32 head, u32 tail,
+        u32 size, u32 space, u32 len)
 {
 }
 
