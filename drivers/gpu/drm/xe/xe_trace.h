67ec217a043439577a3dc12275ff6c19dda28dc2
Radhakrishna Sripada <radhakrishna.sripada@intel.com>
2024-06-07 11:29:39 -0700

    drm/xe/trace: Extract guc related traces
    xe_trace.h is starting to get over crowded. Move the traces
    related to guc to its own file.
    
    v2: Update year in License(Gustavo)
    
    Reviewed-by: Gustavo Sousa <gustavo.sousa@intel.com>
    Suggested-by: Jani Nikula <jani.nikula@intel.com>
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Signed-off-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240607182943.3572524-3-radhakrishna.sripada@intel.com

--- drivers/gpu/drm/xe/xe_trace.h
+++ drivers/gpu/drm/xe/xe_trace.h
@@ -70,26 +70,6 @@ trace_xe_hw_fence_try_signal(struct xe_hw_fence *fence)
 }
 
 static inline void
-trace_xe_guc_ctb_g2h(u32 head, u32 tail, u32 size, u32 space, u32 len)
-{
-}
-
-static inline void
-trace_xe_guc_ctb_h2g(u32 head, u32 tail, u32 size, u32 space, u32 len)
-{
-}
-
-static inline void
-trace_xe_guc_ct_g2h_flow_control(u32 head, u32 tail, u32 size, u32 space, u32 len)
-{
-}
-
-static inline void
-trace_xe_guc_ct_h2g_flow_control(u32 head, u32 tail, u32 size, u32 space, u32 len)
-{
-}
-
-static inline void
 trace_xe_exec_queue_create(struct xe_exec_queue *q)
 {
 }
