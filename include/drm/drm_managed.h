7af5f57156433ce1e16b6636e4bcab2ac7b1ea30
Lutz Bichler <Lutz.Bichler@gmail.com>
2024-06-16 18:31:19 +0200

    drm: Correct return type of drmm_mutex_init

--- include/drm/drm_managed.h
+++ include/drm/drm_managed.h
@@ -21,11 +21,11 @@ void drmm_release_action(struct drm_device *, drmm_func_t, void *);
 
 void drmm_mutex_release(struct drm_device *, void *);
 
-static inline void
+static inline int
 drmm_mutex_init(struct drm_device *dev, struct mutex *m)
 {
     mutex_init(m);
-    drmm_add_action_or_reset(dev, drmm_mutex_release, m);
+    return drmm_add_action_or_reset(dev, drmm_mutex_release, m);
 }
 
 #endif
