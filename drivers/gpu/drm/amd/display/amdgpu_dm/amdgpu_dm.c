d851cbfd8d90f075ee8f7abf67009397d8e3df89
Tom Chung <chiahsuan.chung@amd.com>
2024-06-17 15:59:06 +0800

    drm/amd/display: Reset freesync config before update new state
    [Why]
    Sometimes the new_crtc_state->vrr_infopacket did not sync up with the
    current state.
    It will affect the update_freesync_state_on_stream() does not update
    the state correctly.
    
    [How]
    Reset the freesync config before get_freesync_config_for_crtc() to
    make sure we have the correct new_crtc_state for VRR.
    
    Reviewed-by: Sun peng Li <sunpeng.li@amd.com>
    Signed-off-by: Jerry Zuo <jerry.zuo@amd.com>
    Signed-off-by: Tom Chung <chiahsuan.chung@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -9974,6 +9974,7 @@ skip_modeset:
 	}
 
 	/* Update Freesync settings. */
+	reset_freesync_config_for_crtc(dm_new_crtc_state);
 	get_freesync_config_for_crtc(dm_new_crtc_state,
 				     dm_new_conn_state);
 
