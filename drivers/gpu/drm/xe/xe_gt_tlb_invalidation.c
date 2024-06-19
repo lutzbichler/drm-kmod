9be95d0073e80ca27c1bf4fe5586597772bb69ed
Michal Wajdeczko <michal.wajdeczko@intel.com>
2024-06-19 23:45:55 +0200

    drm/xe/vf: Don't use register based TLB invalidation if VF
    VF drivers can only use GuC-based TLB invalidation, as they don't
    have access to the related registers. However, VFs shouldn't need
    any explicit TLB invalidation before enabling CTB communication,
    as there will be an implicit GGTT TLB invalidation issued by the
    GuC itself as part of MMIO-based action handling.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Reviewed-by: Piotr Piórkowski <piotr.piorkowski@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240619214557.905-8-michal.wajdeczko@intel.com

--- drivers/gpu/drm/xe/xe_gt_tlb_invalidation.c
+++ drivers/gpu/drm/xe/xe_gt_tlb_invalidation.c
@@ -13,6 +13,7 @@
 #include "xe_guc.h"
 #include "xe_guc_ct.h"
 #include "xe_mmio.h"
+#include "xe_sriov.h"
 #include "xe_trace.h"
 #ifdef __FreeBSD__
 #include "xe_vm.h"
@@ -252,6 +253,9 @@ int xe_gt_tlb_invalidation_ggtt(struct xe_gt *gt)
 
 		xe_gt_tlb_invalidation_wait(gt, seqno);
 	} else if (xe_device_uc_enabled(xe) && !xe_device_wedged(xe)) {
+		if (IS_SRIOV_VF(xe))
+			return 0;
+
 		xe_gt_WARN_ON(gt, xe_force_wake_get(gt_to_fw(gt), XE_FW_GT));
 		if (xe->info.platform == XE_PVC || GRAPHICS_VER(xe) >= 20) {
 			xe_mmio_write32(gt, PVC_GUC_TLB_INV_DESC1,
