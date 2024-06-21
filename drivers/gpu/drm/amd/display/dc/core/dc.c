193521e7515c03c6b0174c9cd1910df2f761a16e
Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
2024-06-21 16:11:28 -0400

    drm/amd/display: Notify DMCUB of D0/D3 state
    [Why]
    We want to avoid arming the HPD timer in firmware when preparing for
    S0i3 entry when DC is considered in D3.
    
    [How]
    Notify DMCUB of the power state transitions so it can decide to arm
    the HPD timer for idle in DCN35 only in D0.
    
    Reviewed-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Ovidiu Bunea <Ovidiu.Bunea@amd.com>
    Signed-off-by: Zaeem Mohamed <zaeem.mohamed@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc.c
+++ drivers/gpu/drm/amd/display/dc/core/dc.c
@@ -5163,6 +5163,8 @@ void dc_set_power_state(struct dc *dc, enum dc_acpi_cm_power_state power_state)
 
 		dc_z10_restore(dc);
 
+		dc_dmub_srv_notify_fw_dc_power_state(dc->ctx->dmub_srv, power_state);
+
 		dc->hwss.init_hw(dc);
 
 		if (dc->hwss.init_sys_ctx != NULL &&
@@ -5173,6 +5175,8 @@ void dc_set_power_state(struct dc *dc, enum dc_acpi_cm_power_state power_state)
 		break;
 	default:
 		ASSERT(dc->current_state->stream_count == 0);
+
+		dc_dmub_srv_notify_fw_dc_power_state(dc->ctx->dmub_srv, power_state);
 
 		dc_state_destruct(dc->current_state);
 
