350088424125353fc27e50ba0799e8fa6d8f16c3
Thomas Zimmermann <tzimmermann@suse.de>
2024-09-24 09:13:01 +0200

    drm/fbdev-shmem: Remove obsolete setup function
    The old setup function drm_fbdev_shmem_setup() is unused. Remove it
    and its internal callbacks. New drivers should call drm_client_setup()
    instead.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Javier Martinez Canillas <javierm@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240924071734.98201-64-tzimmermann@suse.de

--- include/drm/drm_fbdev_shmem.h
+++ include/drm/drm_fbdev_shmem.h
@@ -2,7 +2,6 @@
 
 #define DRM_FBDEV_SHMEM_H
 
-struct drm_device;
 struct drm_fb_helper;
 struct drm_fb_helper_surface_size;
 
@@ -12,12 +11,7 @@ int drm_fbdev_shmem_driver_fbdev_probe(struct drm_fb_helper *fb_helper,
 
 #define DRM_FBDEV_SHMEM_DRIVER_OPS \
        .fbdev_probe = drm_fbdev_shmem_driver_fbdev_probe
-
-void drm_fbdev_shmem_setup(struct drm_device *dev, unsigned int preferred_bpp);
 #else
-static inline void drm_fbdev_shmem_setup(struct drm_device *dev, unsigned int preferred_bpp)
-{ }
-
 #define DRM_FBDEV_SHMEM_DRIVER_OPS \
        .fbdev_probe = NULL
 #endif
