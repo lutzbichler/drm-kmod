261d89d87bc3c277de4b44ca175c1614b44f4a68
Thomas Zimmermann <tzimmermann@suse.de>
2024-09-24 09:12:47 +0200

    drm/fbdev-shmem: Support struct drm_driver.fbdev_probe
    Rework fbdev probing to support fbdev_probe in struct drm_driver
    and reimplement the old fb_probe callback on top of it. Provide an
    initializer macro for struct drm_driver that sets the callback
    according to the kernel configuration.
    
    This change allows the common fbdev client to run on top of SHMEM-
    based DRM drivers.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Javier Martinez Canillas <javierm@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240924071734.98201-50-tzimmermann@suse.de

--- include/drm/drm_fbdev_shmem.h
+++ include/drm/drm_fbdev_shmem.h
@@ -1,15 +1,37 @@
 /* SPDX-License-Identifier: MIT */
 
+<<<<<<< HEAD
 #ifndef DRM_FBDEV_SHMEM_H
 #define DRM_FBDEV_SHMEM_H
 
 struct drm_device;
 
 #ifdef CONFIG_DRM_FBDEV_EMULATION
+=======
+#define DRM_FBDEV_SHMEM_H
+
+struct drm_device;
+struct drm_fb_helper;
+struct drm_fb_helper_surface_size;
+
+#ifdef CONFIG_DRM_FBDEV_EMULATION
+int drm_fbdev_shmem_driver_fbdev_probe(struct drm_fb_helper *fb_helper,
+                                      struct drm_fb_helper_surface_size *sizes);
+
+#define DRM_FBDEV_SHMEM_DRIVER_OPS \
+       .fbdev_probe = drm_fbdev_shmem_driver_fbdev_probe
+
+>>>>>>> drm/fbdev-shmem: Support struct drm_driver.fbdev_probe
 void drm_fbdev_shmem_setup(struct drm_device *dev, unsigned int preferred_bpp);
 #else
 static inline void drm_fbdev_shmem_setup(struct drm_device *dev, unsigned int preferred_bpp)
 { }
+<<<<<<< HEAD
+=======
+
+#define DRM_FBDEV_SHMEM_DRIVER_OPS \
+       .fbdev_probe = NULL
+>>>>>>> drm/fbdev-shmem: Support struct drm_driver.fbdev_probe
 #endif
 
 #endif
