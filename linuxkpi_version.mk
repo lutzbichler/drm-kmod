1de12a80cfc7af16f6fd269025932825f35db270
Frederic Weisbecker <frederic@kernel.org>
2024-09-27 00:49:07 +0200

    treewide: Introduce kthread_run_worker[_on_cpu]()
    kthread_create() creates a kthread without running it yet. kthread_run()
    creates a kthread and runs it.
    
    On the other hand, kthread_create_worker() creates a kthread worker and
    runs it.
    
    This difference in behaviours is confusing. Also there is no way to
    create a kthread worker and affine it using kthread_bind_mask() or
    kthread_affine_preferred() before starting it.
    
    Consolidate the behaviours and introduce kthread_run_worker[_on_cpu]()
    that behaves just like kthread_run(). kthread_create_worker[_on_cpu]()
    will now only create a kthread worker without starting it.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- linuxkpi_version.mk
+++ linuxkpi_version.mk
@@ -4,7 +4,7 @@
 # the source tree and external components such as
 # nvidia-drm
 
-LINUXKPI_VERSION_NUMBER=	61300
+LINUXKPI_VERSION_NUMBER=	61400
 
 CFLAGS+=	-DLINUXKPI_VERSION=${LINUXKPI_VERSION_NUMBER}
 
