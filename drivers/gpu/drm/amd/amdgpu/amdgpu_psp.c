f4b18c1fd3e294e7ee2a4cdc52c2966e8e81700c
Boyuan Zhang <boyuan.zhang@amd.com>
2024-09-29 15:17:51 -0400

    drm/amdgpu: pass ip_block in set_powergating_state
    Pass ip_block instead of adev in set_powergating_state callback function.
    Modify set_powergating_state ip functions for all correspoding ip blocks.
    
    v2: fix a ip block index error.
    
    v3: remove type casting
    
    Signed-off-by: Boyuan Zhang <boyuan.zhang@amd.com>
    Suggested-by: Christian König <christian.koenig@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Sunil Khatri <sunil.khatri@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
@@ -3859,7 +3859,7 @@ static int psp_set_clockgating_state(void *handle,
 	return 0;
 }
 
-static int psp_set_powergating_state(void *handle,
+static int psp_set_powergating_state(struct amdgpu_ip_block *ip_block,
 				     enum amd_powergating_state state)
 {
 	return 0;
