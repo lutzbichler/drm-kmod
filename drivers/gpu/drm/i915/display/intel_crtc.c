19feffe795eef31acee81e271777ad4d923c15d7
Ville Syrjälä <ville.syrjala@linux.intel.com>
2024-12-10 23:10:05 +0200

    drm/i915/vrr: Plumb the DSB into intel_vrr_send_push()
    Plumb the DSB down into intel_vrr_send_push() so that we can
    perform the opration on the DSB.
    
    TRANS_PUSH, being a transcoder register, needs non-posted writes
    to make it through.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241210211007.5976-17-ville.syrjala@linux.intel.com
    Reviewed-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>

--- drivers/gpu/drm/i915/display/intel_crtc.c
+++ drivers/gpu/drm/i915/display/intel_crtc.c
@@ -720,7 +720,7 @@ void intel_pipe_update_end(struct intel_atomic_state *state,
 	 * which would cause the next frame to terminate already at vmin
 	 * vblank start instead of vmax vblank start.
 	 */
-	intel_vrr_send_push(new_crtc_state);
+	intel_vrr_send_push(NULL, new_crtc_state);
 
 #ifdef __linux__
 	local_irq_enable();
