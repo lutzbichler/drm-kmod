55427cc2b47f9064e35fd4d93696453f7768c04a
Ville Syrjälä <ville.syrjala@linux.intel.com>
2024-12-18 19:36:50 +0200

    drm/i915: Include pixel format in plane tracepoints MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    Make debugging a bit easier by including the pixel format in
    the plane tracepoints.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241218173650.19782-5-ville.syrjala@linux.intel.com
    Reviewed-by: Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>

--- drivers/gpu/drm/i915/display/intel_display_trace.h
+++ drivers/gpu/drm/i915/display/intel_display_trace.h
@@ -216,10 +216,11 @@ trace_vlv_fifo_size(struct intel_crtc *crtc, u32 sprite0_start, u32 sprite1_star
 static inline void
 trace_intel_plane_update_noarm(const struct intel_plane_state *plane_state, struct intel_crtc *crtc)
 {
-	CTR5(KTR_DRM,
-	    "intel_plane_update_noarm[1/3]: dev %s, pipe %c %s, frame=%u, scanline=%u",
+	CTR6(KTR_DRM,
+	    "intel_plane_update_noarm[1/3]: dev %s, pipe %c %s, frame=%u, scanline=%u, format=%p4cc",
 	    __dev_name_kms(plane_state->uapi.plane), pipe_name(crtc->pipe), plane->base.name,
-	    intel_crtc_get_vblank_counter(crtc), intel_get_crtc_scanline(crtc));
+	    intel_crtc_get_vblank_counter(crtc), intel_get_crtc_scanline(crtc),
+	    &plane_state->hw.fb->format->format);
 	/* FIXME FreeBSD
 	CTR8(KTR_DRM,
 	    "intel_plane_update_noarm[2/3]: " DRM_RECT_FP_FMT " ->",
@@ -233,10 +234,11 @@ static inline void
 trace_intel_plane_update_arm(const struct intel_plane_state *plane_state,
 	struct intel_crtc *crtc)
 {
-	CTR5(KTR_DRM,
-	    "intel_plane_update_arm[1/3]: dev %s, pipe %c %s, frame=%u, scanline=%u",
+	CTR6(KTR_DRM,
+	    "intel_plane_update_arm[1/3]: dev %s, pipe %c %s, frame=%u, scanline=%u, format=%p4cc",
 	    __dev_name_kms(plane_state->uapi.plane), pipe_name(crtc->pipe), plane->base.name,
-	    intel_crtc_get_vblank_counter(crtc), intel_get_crtc_scanline(crtc));
+	    intel_crtc_get_vblank_counter(crtc), intel_get_crtc_scanline(crtc),
+	    &plane_state->hw.fb->format->format);
 	/* FIXME FreeBSD
 	CTR8(KTR_DRM,
 	    "intel_plane_update_arm[2/3]: " DRM_RECT_FP_FMT " ->",
