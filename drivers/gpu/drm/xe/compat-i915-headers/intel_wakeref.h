a24d7fef1bb8cd56a46e195be2221eabc476f374
Lutz Bichler <Lutz.Bichler@gmail.com>
2024-12-24 13:52:57 +0100

    drm/xe: Import driver (non-functional for now)

---
+++ drivers/gpu/drm/xe/compat-i915-headers/intel_wakeref.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2023 Intel Corporation
+ */
+
+#include <linux/types.h>
+
+#ifndef _COMPAT_I915_INTEL_WAKEREF_H_
+#define _COMPAT_I915_INTEL_WAKEREF_H_
+
+typedef unsigned long intel_wakeref_t;
+
+#endif /* _COMPAT_I915_INTEL_WAKEREF_H_ */
+
