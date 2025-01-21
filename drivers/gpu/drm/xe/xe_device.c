cb04cf5866f66a89b501e0eb1d876a2ee6982217
Maarten Lankhorst <dev@lankhorst.se>
2025-01-21 15:28:50 +0100

    drm/xe/display: Use a single early init call for display
    Now that interrupts are disabled for xe_display_init_noaccel,
    both xe_display_init_noirq and xe_display_init_noaccel run in the same
    context.
    
    This means that we can get rid of the 3 different init calls. Without
    interrupts, nothing is touching display up to this point.
    Unify those 3 early display calls into a single xe_display_init_early(),
    this makes the init sequence cleaner, and display less tangled during
    init.
    
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250121142850.4960-3-dev@lankhorst.se
    Signed-off-by: Maarten Lankhorst <dev@lankhorst.se>

--- drivers/gpu/drm/xe/xe_device.c
+++ drivers/gpu/drm/xe/xe_device.c
@@ -762,10 +762,6 @@ int xe_device_probe(struct xe_device *xe)
 		return err;
 
 	xe->info.mem_region_mask = 1;
-	err = xe_display_init_nommio(xe);
-	if (err)
-		return err;
-
 	err = xe_set_dma_info(xe);
 	if (err)
 		return err;
@@ -820,10 +816,6 @@ int xe_device_probe(struct xe_device *xe)
 	if (err)
 		return err;
 
-	err = xe_display_init_noirq(xe);
-	if (err)
-		return err;
-
 	err = probe_has_flat_ccs(xe);
 	if (err)
 		goto err;
@@ -847,7 +839,7 @@ int xe_device_probe(struct xe_device *xe)
 	 * This is the reason the first allocation needs to be done
 	 * inside display.
 	 */
-	err = xe_display_init_noaccel(xe);
+	err = xe_display_init_early(xe);
 	if (err)
 		goto err;
 
