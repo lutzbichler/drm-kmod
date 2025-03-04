8ed79923ba0eea9b8240aadcec56962cd35e2fcb
Thomas Hellström <thomas.hellstrom@linux.intel.com>
2025-03-04 18:33:42 +0100

    drm/xe/userptr: Unmap userptrs in the mmu notifier
    If userptr pages are freed after a call to the xe mmu notifier,
    the device will not be blocked out from theoretically accessing
    these pages unless they are also unmapped from the iommu, and
    this violates some aspects of the iommu-imposed security.
    
    Ensure that userptrs are unmapped in the mmu notifier to
    mitigate this. A naive attempt would try to free the sg table, but
    the sg table itself may be accessed by a concurrent bind
    operation, so settle for only unmapping.
    
    v3:
    - Update lockdep asserts.
    - Fix a typo (Matthew Auld)
    
    Fixes: 81e058a3e7fd ("drm/xe: Introduce helper to populate userptr")
    Cc: Oak Zeng <oak.zeng@intel.com>
    Cc: Matthew Auld <matthew.auld@intel.com>
    Cc: <stable@vger.kernel.org> # v6.10+
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Acked-by: Matthew Brost <matthew.brost@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250304173342.22009-4-thomas.hellstrom@linux.intel.com
    (cherry picked from commit ba767b9d01a2c552d76cf6f46b125d50ec4147a6)
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/xe/xe_vm.c
+++ drivers/gpu/drm/xe/xe_vm.c
@@ -630,6 +630,8 @@ static void __vma_userptr_invalidate(struct xe_vm *vm, struct xe_userptr_vma *uv
 		err = xe_vm_invalidate_vma(vma);
 		XE_WARN_ON(err);
 	}
+
+	xe_hmm_userptr_unmap(uvma);
 }
 
 static bool vma_userptr_invalidate(struct mmu_interval_notifier *mni,
@@ -1052,6 +1054,7 @@ static struct xe_vma *xe_vma_create(struct xe_vm *vm,
 			INIT_LIST_HEAD(&userptr->invalidate_link);
 			INIT_LIST_HEAD(&userptr->repin_link);
 			vma->gpuva.gem.offset = bo_offset_or_userptr;
+			mutex_init(&userptr->unmap_mutex);
 
 #ifdef __linux__
 			err = mmu_interval_notifier_insert(&userptr->notifier,
@@ -1085,10 +1088,9 @@ static void xe_vma_destroy_late(struct xe_vma *vma)
 	}
 
 	if (xe_vma_is_userptr(vma)) {
-#ifdef __linux__
 		struct xe_userptr_vma *uvma = to_userptr_vma(vma);
 		struct xe_userptr *userptr = &uvma->userptr;
-
+#ifdef __linux__
 		if (userptr->sg)
 			xe_hmm_userptr_free_sg(uvma);
 
@@ -1099,6 +1101,7 @@ static void xe_vma_destroy_late(struct xe_vma *vma)
 		 */
 		mmu_interval_notifier_remove(&userptr->notifier);
 #endif
+		mutex_destroy(&userptr->unmap_mutex);
 		xe_vm_put(vm);
 	} else if (xe_vma_is_null(vma)) {
 		xe_vm_put(vm);
