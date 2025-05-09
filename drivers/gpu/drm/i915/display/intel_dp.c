5a797d112ac2aff7a48779adef32e95ee2581a9c
Imre Deak <imre.deak@intel.com>
2025-05-09 21:03:28 +0300

    drm/i915/dp_mst: Use the correct connector while computing the link BPP limit on MST
    Atm, on an MST link in DSC mode
    intel_dp_compute_config_link_bpp_limits() calculates the maximum link
    bpp limit using the MST root connector's DSC capabilities. That's not
    correct in general: the decompression could be performed by a branch
    device downstream of the root branch device or the sink itself.
    
    Fix the above by passing to intel_dp_compute_config_link_bpp_limits()
    the actual connector being modeset, containing the correct DSC
    capabilities.
    
    Cc: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    Fixes: 1c5b72daff46 ("drm/i915/dp: Set the DSC link limits in intel_dp_compute_config_link_bpp_limits")
    Reviewed-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    Reviewed-by: Luca Coelho <luciano.coelho@intel.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Link: https://lore.kernel.org/r/20250509180340.554867-2-imre.deak@intel.com
    (cherry picked from commit 266e2fcfe2ea0d062ea392cd22f6250ae0d11c04)
    Signed-off-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>

--- drivers/gpu/drm/i915/display/intel_dp.c
+++ drivers/gpu/drm/i915/display/intel_dp.c
@@ -2528,6 +2528,7 @@ intel_dp_dsc_compute_pipe_bpp_limits(struct intel_dp *intel_dp,
 
 bool
 intel_dp_compute_config_limits(struct intel_dp *intel_dp,
+			       struct intel_connector *connector,
 			       struct intel_crtc_state *crtc_state,
 			       bool respect_downstream_limits,
 			       bool dsc,
@@ -2581,7 +2582,7 @@ intel_dp_compute_config_limits(struct intel_dp *intel_dp,
 	intel_dp_test_compute_config(intel_dp, crtc_state, limits);
 
 	return intel_dp_compute_config_link_bpp_limits(intel_dp,
-						       intel_dp->attached_connector,
+						       connector,
 						       crtc_state,
 						       dsc,
 						       limits);
@@ -2642,7 +2643,7 @@ intel_dp_compute_link_config(struct intel_encoder *encoder,
 	joiner_needs_dsc = intel_dp_joiner_needs_dsc(display, num_joined_pipes);
 
 	dsc_needed = joiner_needs_dsc || intel_dp->force_dsc_en ||
-		     !intel_dp_compute_config_limits(intel_dp, pipe_config,
+		     !intel_dp_compute_config_limits(intel_dp, connector, pipe_config,
 						     respect_downstream_limits,
 						     false,
 						     &limits);
@@ -2676,7 +2677,7 @@ intel_dp_compute_link_config(struct intel_encoder *encoder,
 			    str_yes_no(ret), str_yes_no(joiner_needs_dsc),
 			    str_yes_no(intel_dp->force_dsc_en));
 
-		if (!intel_dp_compute_config_limits(intel_dp, pipe_config,
+		if (!intel_dp_compute_config_limits(intel_dp, connector, pipe_config,
 						    respect_downstream_limits,
 						    true,
 						    &limits))
