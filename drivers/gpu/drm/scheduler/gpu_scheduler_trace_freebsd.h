128ba66581a96041f5350ed218ab2dcd7f01d93b
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2025-05-26 14:54:49 +0200

    drm/sched: Cleanup event names
    All events now start with the same prefix (drm_sched_job_).
    
    drm_sched_job_wait_dep was misleading because it wasn't waiting
    at all. It's now replaced by trace_drm_sched_job_unschedulable,
    which is only traced if the job cannot be scheduled.
    For moot dependencies, nothing is traced.
    
    Signed-off-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Signed-off-by: Philipp Stanner <phasta@kernel.org>
    Link: https://lore.kernel.org/r/20250526125505.2360-8-pierre-eric.pelloux-prayer@amd.com

--- drivers/gpu/drm/scheduler/gpu_scheduler_trace_freebsd.h
+++ drivers/gpu/drm/scheduler/gpu_scheduler_trace_freebsd.h
@@ -3,23 +3,23 @@
 #define _GPU_SCHEDULER_TRACE_FREEBSD_H_
 
 static inline void
-trace_drm_sched_job(void *sched_job, void *entity) {
-	CTR2(KTR_DRM, "drm_sched_job %p, entity %p", sched_job, entity);
+trace_drm_sched_job_queue(void *sched_job, void *entity) {
+	CTR2(KTR_DRM, "drm_sched_job_queue %p, entity %p", sched_job, entity);
 }
 
 static inline void
-trace_drm_run_job(void *sched_job, void *entity) {
-	CTR2(KTR_DRM, "drm_sched_job %p, entity %p", sched_job, entity);
+trace_drm_sched_job_run(void *sched_job, void *entity) {
+	CTR2(KTR_DRM, "drm_sched_job_run %p, entity %p", sched_job, entity);
 }
 
 static inline void
-trace_drm_sched_job_wait_dep(struct drm_sched_job *job, struct dma_fence *fence) {
-	CTR2(KTR_DRM, "drm_process_wait_job %p fence %p", job, fence);
+trace_drm_sched_job_unschedulable(struct drm_sched_job *job, struct dma_fence *fence) {
+	CTR2(KTR_DRM, "drm_sched_job_unschedulable %p fence %p", job, fence);
 }
 
 static inline void
-trace_drm_sched_process_job(void *s_fence) {
-	CTR1(KTR_DRM, "drm_process_sched_job %p", s_fence);
+trace_drm_sched_job_done(void *s_fence) {
+	CTR1(KTR_DRM, "drm_sched_job_done %p", s_fence);
 }
 
 static inline void
