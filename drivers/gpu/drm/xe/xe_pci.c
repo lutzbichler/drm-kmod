1929f2ae0abb06939bbbe1dfcebe216eece3d126
Michal Wajdeczko <michal.wajdeczko@intel.com>
2025-07-22 20:26:17 +0200

    drm/xe: Enable SR-IOV for ADL/ATSM
    We were already testing those two platforms for a while on our CI,
    but enabling flag (has_sriov) was only available on the topic branch
    and only for builds with CONFIG_DRM_XE_DEBUG config.
    
    Since those two platforms are guarded by the another enabling flag
    (require_force_probe) and we believe our SR-IOV support for them is
    at sufficient level to start enjoying the feature, turn on the
    SR-IOV enabling flag unconditionally.
    
    Suggested-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Reviewed-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Link: https://lore.kernel.org/r/20250722182618.30811-4-michal.wajdeczko@intel.com
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/xe/xe_pci.c
+++ drivers/gpu/drm/xe/xe_pci.c
@@ -197,6 +197,7 @@ static const struct xe_device_desc adl_s_desc = {
 	.dma_mask_size = 39,
 	.has_display = true,
 	.has_llc = true,
+	.has_sriov = true,
 	.max_gt_per_tile = 1,
 	.require_force_probe = true,
 	.subplatforms = (const struct xe_subplatform_desc[]) {
@@ -214,6 +215,7 @@ static const struct xe_device_desc adl_p_desc = {
 	.dma_mask_size = 39,
 	.has_display = true,
 	.has_llc = true,
+	.has_sriov = true,
 	.max_gt_per_tile = 1,
 	.require_force_probe = true,
 	.subplatforms = (const struct xe_subplatform_desc[]) {
@@ -229,6 +231,7 @@ static const struct xe_device_desc adl_n_desc = {
 	.dma_mask_size = 39,
 	.has_display = true,
 	.has_llc = true,
+	.has_sriov = true,
 	.max_gt_per_tile = 1,
 	.require_force_probe = true,
 };
@@ -274,6 +277,7 @@ static const struct xe_device_desc ats_m_desc = {
 
 	DG2_FEATURES,
 	.has_display = false,
+	.has_sriov = true,
 };
 
 static const struct xe_device_desc dg2_desc = {
