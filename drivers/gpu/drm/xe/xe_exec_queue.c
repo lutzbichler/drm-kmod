2c178695fd396ff1e5fa1b6986b14871ca3a1d1d
Tomasz Lis <tomasz.lis@intel.com>
2025-08-02 05:10:43 +0200

    drm/xe/vf: Post migration, repopulate ring area for  pending request MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    The commands within ring area allocated for a request may contain
    references to GGTT. These references require update after VF
    migration, in order to continue any preempted LRCs, or jobs which
    were emitted to the ring but not sent to GuC yet.
    
    This change calls the emit function again for all such jobs,
    as part of post-migration recovery.
    
    v2: Moved few functions to better files
    v3: Take job_list_lock
    v4: Rephrased comments
    
    Signed-off-by: Tomasz Lis <tomasz.lis@intel.com>
    Cc: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Michal Winiarski <michal.winiarski@intel.com>
    Reviewed-by: Michal Winiarski <michal.winiarski@intel.com>
    Cc: Jonathan Cavitt <jonathan.cavitt@intel.com>
    Reviewed-by: Jonathan Cavitt <jonathan.cavitt@intel.com>
    Link: https://lore.kernel.org/r/20250802031045.1127138-7-tomasz.lis@intel.com
    Signed-off-by: Michał Winiarski <michal.winiarski@intel.com>

--- drivers/gpu/drm/xe/xe_exec_queue.c
+++ drivers/gpu/drm/xe/xe_exec_queue.c
@@ -28,6 +28,11 @@
 #include "xe_vm.h"
 #include "xe_pxp.h"
 
+#ifdef __FreeBSD__
+#include "xe_guc_exec_queue_types.h"
+#include "xe_sched_job.h"
+#endif
+
 enum xe_exec_queue_sched_prop {
 	XE_EXEC_QUEUE_JOB_TIMEOUT = 0,
 	XE_EXEC_QUEUE_TIMESLICE = 1,
@@ -1099,4 +1104,28 @@ void xe_exec_queue_contexts_hwsp_rebase(struct xe_exec_queue *q, void *scratch)
 		xe_lrc_update_memirq_regs_with_address(q->lrc[i], q->hwe, scratch);
 		xe_lrc_update_hwctx_regs_with_address(q->lrc[i]);
 	}
+}
+
+/**
+ * xe_exec_queue_jobs_ring_restore - Re-emit ring commands of requests pending on given queue.
+ * @q: the &xe_exec_queue struct instance
+ */
+void xe_exec_queue_jobs_ring_restore(struct xe_exec_queue *q)
+{
+	struct xe_gpu_scheduler *sched = &q->guc->sched;
+	struct xe_sched_job *job;
+
+	/*
+	 * This routine is used within VF migration recovery. This means
+	 * using the lock here introduces a restriction: we cannot wait
+	 * for any GFX HW response while the lock is taken.
+	 */
+	spin_lock(&sched->base.job_list_lock);
+	list_for_each_entry(job, &sched->base.pending_list, drm.list) {
+		if (xe_sched_job_is_error(job))
+			continue;
+
+		q->ring_ops->emit_job(job);
+	}
+	spin_unlock(&sched->base.job_list_lock);
 }
