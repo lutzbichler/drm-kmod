1a037d8745a04c1abd330672dcb3c9c742f7a2ee
Lutz Bichler <Lutz.Bichler@gmail.com>
2025-08-23 18:53:46 +0200

    drm/drm_managed; Add __drm_workqueue_release

--- include/drm/drm_managed.h
+++ include/drm/drm_managed.h
@@ -20,6 +20,7 @@ int drmm_add_action_or_reset(struct drm_device *, drmm_func_t, void *);
 void drmm_release_action(struct drm_device *, drmm_func_t, void *);
 
 void drmm_mutex_release(struct drm_device *, void *);
+void __drmm_workqueue_release(struct drm_device *, void *);
 
 static inline int
 drmm_mutex_init(struct drm_device *dev, struct mutex *m)
