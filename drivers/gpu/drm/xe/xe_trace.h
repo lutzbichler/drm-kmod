db780c9840271ac29f4791c5144a80bc7f5e6774
Matthew Brost <matthew.brost@intel.com>
2025-08-26 18:29:08 +0000

    drm/xe: Decouple TLB invalidations from GT
    Decouple TLB invalidations from the GT by updating the TLB invalidation
    layer to accept a `struct xe_tlb_inval` instead of a `struct xe_gt`.
    Also, rename *gt_tlb* to *tlb*. The internals of the TLB invalidation
    code still operate on a GT, but this is now hidden from the rest of the
    driver.
    
    Signed-off-by: Stuart Summers <stuart.summers@intel.com>
    Reviewed-by: Stuart Summers <stuart.summers@intel.com>
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>
    Link: https://lore.kernel.org/r/20250826182911.392550-7-stuart.summers@intel.com

--- drivers/gpu/drm/xe/xe_trace.h
+++ drivers/gpu/drm/xe/xe_trace.h
@@ -7,42 +7,42 @@
 #include <sys/ktr.h>
 
 #include "xe_gpu_scheduler_types.h"
-#include "xe_gt_tlb_inval_types.h"
+#include "xe_tlb_inval_types.h"
 #include "xe_sched_job_types.h"
 #include "xe_vm_types.h"
 
 static inline void
-trace_xe_gt_tlb_inval_fence_timeout(struct xe_device *xe, struct xe_gt_tlb_inval_fence *fence)
+trace_xe_tlb_inval_fence_timeout(struct xe_device *xe, struct xe_tlb_inval_fence *fence)
 {
 }
 
 static inline void
-trace_xe_gt_tlb_inval_fence_signal(struct xe_device *xe, struct xe_gt_tlb_inval_fence *fence)
+trace_xe_tlb_inval_fence_signal(struct xe_device *xe, struct xe_tlb_inval_fence *fence)
 {
 }
 
 static inline void
-trace_xe_gt_tlb_inval_fence_send(struct xe_device *xe, struct xe_gt_tlb_inval_fence *fence)
+trace_xe_tlb_inval_fence_send(struct xe_device *xe, struct xe_tlb_inval_fence *fence)
 {
 }
 
 static inline void
-trace_xe_gt_tlb_inval_fence_recv(struct xe_device *xe, struct xe_gt_tlb_inval_fence *fence)
+trace_xe_tlb_inval_fence_recv(struct xe_device *xe, struct xe_tlb_inval_fence *fence)
 {
 }
 
 static inline void
-trace_xe_gt_tlb_inval_fence_create(struct xe_device *xe, struct xe_gt_tlb_inval_fence *fence)
+trace_xe_tlb_inval_fence_create(struct xe_device *xe, struct xe_tlb_inval_fence *fence)
 {
 }
 
 static inline void
-trace_xe_gt_tlb_inval_fence_work_func(struct xe_device *xe, struct xe_gt_tlb_inval_fence *fence)
+trace_xe_tlb_inval_fence_work_func(struct xe_device *xe, struct xe_tlb_inval_fence *fence)
 {
 }
 
 static inline void
-trace_xe_gt_tlb_inval_fence_cb(struct xe_device *xe, struct xe_gt_tlb_inval_fence *fence)
+trace_xe_tlb_inval_fence_cb(struct xe_device *xe, struct xe_tlb_inval_fence *fence)
 {
 }
 
