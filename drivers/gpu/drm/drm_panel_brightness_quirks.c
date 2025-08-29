80fe9b65c9391afa1b48425c2909ef786d56fa06
Antheas Kapenekakis <lkml@antheas.dev>
2025-08-29 16:55:37 +0200

    drm: panel-backlight-quirks: Convert brightness quirk to generic structure MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    Currently, the brightness quirk is limited to minimum brightness only.
    Refactor it to a structure, so that more quirks can be added in the
    future. Reserve 0 value for "no quirk", and use u16 to allow minimum
    brightness up to 255.
    
    Tested-by: Philip Müller <philm@manjaro.org>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>
    Link: https://lore.kernel.org/r/20250829145541.512671-3-lkml@antheas.dev
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Mario Limonciello (AMD) <superm1@kernel.org>

--- drivers/gpu/drm/drm_panel_brightness_quirks.c
+++ drivers/gpu/drm/drm_panel_brightness_quirks.c
@@ -1,9 +1,12 @@
 /* Public domain */
 
+#include <linux/err.h>
+#include <linux/errno.h>
+
 #include <drm/drm_utils.h>
 
-int
-drm_get_panel_min_brightness_quirk(const struct drm_edid *edid)
+const struct drm_panel_backlight_quirk *
+drm_get_panel_backlight_quirk(const struct drm_edid *edid)
 {
-	return (-1);
+	return (ERR_PTR(-ENOTSUPP));
 }
