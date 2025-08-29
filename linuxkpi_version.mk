891ff67f93554cf5cd4d1a23b3a98a9cdcc0af65
Jani Nikula <jani.nikula@intel.com>
2025-08-29 20:46:01 +0300

    overflow: add range_overflows() and range_end_overflows() MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    Move the range_overflows() and range_end_overflows() along with the _t
    variants over from drm/i915 and drm/buddy to overflow.h.
    
    Cc: Kees Cook <kees@kernel.org>
    Cc: "Gustavo A. R. Silva" <gustavoars@kernel.org>
    Cc: linux-hardening@vger.kernel.org
    Reviewed-by: Kees Cook <kees@kernel.org>
    Reviewed-by: Jouni Högander <jouni.hogander@intel.com>
    Acked-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://lore.kernel.org/r/20250829174601.2163064-3-jani.nikula@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- linuxkpi_version.mk
+++ linuxkpi_version.mk
@@ -4,7 +4,7 @@
 # the source tree and external components such as
 # nvidia-drm
 
-LINUXKPI_VERSION_NUMBER=	61702
+LINUXKPI_VERSION_NUMBER=	61800
 
 CFLAGS+=	-DLINUXKPI_VERSION=${LINUXKPI_VERSION_NUMBER}
 
