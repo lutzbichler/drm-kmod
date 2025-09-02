0470e550d0f4636d4fa82acf72f571ec1ef4ccfd
Jani Nikula <jani.nikula@intel.com>
2025-09-02 20:51:51 +0300

    drm/{i915,xe}/panic: rename struct {i915,xe}_panic_data to struct intel_panic
    Prepare for better shared interfaces between panic implementations. The
    struct intel_panic remains an opaque data type, with unique
    implementations in i915 and xe.
    
    This allows us to change the panic data pointer from void * to struct
    intel_panic *, helping type safety.
    
    Cc: Jocelyn Falempe <jfalempe@redhat.com>
    Cc: Maarten Lankhorst <dev@lankhorst.se>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Jocelyn Falempe <jfalempe@redhat.com>
    Link: https://lore.kernel.org/r/884ffc80c8b5fef1b92956e644a4e559560cc2ba.1756835342.git.jani.nikula@intel.com

--- drivers/gpu/drm/i915/display/intel_display_types.h
+++ drivers/gpu/drm/i915/display/intel_display_types.h
@@ -63,6 +63,7 @@ struct intel_ddi_buf_trans;
 struct intel_fbc;
 struct intel_global_objs_state;
 struct intel_hdcp_shim;
+struct intel_panic;
 struct intel_tc_port;
 
 /*
@@ -152,7 +153,7 @@ struct intel_framebuffer {
 	unsigned int vtd_guard;
 
 	unsigned int (*panic_tiling)(unsigned int x, unsigned int y, unsigned int width);
-	void *panic;
+	struct intel_panic *panic;
 };
 
 enum intel_hotplug_state {
