4445d10ded11753240aa1819e86b8ab5c621a5d4
Jani Nikula <jani.nikula@intel.com>
2025-09-02 20:51:47 +0300

    drm/i915/fb: add intel_framebuffer_alloc()
    Add intel_framebuffer_alloc() to hide intel_bo_alloc_framebuffer(), as
    that doesn't feel like the correct abstraction.
    
    Cc: Jocelyn Falempe <jfalempe@redhat.com>
    Cc: Maarten Lankhorst <dev@lankhorst.se>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Jocelyn Falempe <jfalempe@redhat.com>
    Link: https://lore.kernel.org/r/379c306c692c50f6af3b6f2488c213f12627954f.1756835342.git.jani.nikula@intel.com

--- drivers/gpu/drm/i915/display/intel_fb.c
+++ drivers/gpu/drm/i915/display/intel_fb.c
@@ -2343,6 +2343,11 @@ intel_user_framebuffer_create(struct drm_device *dev,
 	return fb;
 }
 
+struct intel_framebuffer *intel_framebuffer_alloc(void)
+{
+	return intel_bo_alloc_framebuffer();
+}
+
 struct drm_framebuffer *
 intel_framebuffer_create(struct drm_gem_object *obj,
 			 const struct drm_format_info *info,
@@ -2351,7 +2356,7 @@ intel_framebuffer_create(struct drm_gem_object *obj,
 	struct intel_framebuffer *intel_fb;
 	int ret;
 
-	intel_fb = intel_bo_alloc_framebuffer();
+	intel_fb = intel_framebuffer_alloc();
 	if (!intel_fb)
 		return ERR_PTR(-ENOMEM);
 
