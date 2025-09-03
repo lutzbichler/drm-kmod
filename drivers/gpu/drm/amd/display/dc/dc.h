856d339039aaf5f2b7d142b10aa0febb42740bb7
Ilya Bakoulin <Ilya.Bakoulin@amd.com>
2025-09-03 14:07:41 -0400

    drm/amd/display: add new block sequence-building/executing functions
    [Why/How]
    Create functions for building/executing HW block programming steps
    
    Reviewed-by: Alvin Lee <alvin.lee2@amd.com>
    Signed-off-by: Ilya Bakoulin <Ilya.Bakoulin@amd.com>
    Signed-off-by: Aurabindo Pillai <aurabindo.pillai@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dc.h
+++ drivers/gpu/drm/amd/display/dc/dc.h
@@ -1165,6 +1165,7 @@ struct dc_debug_options {
 	short auxless_alpm_lfps_t1t2_offset_us;
 	bool disable_stutter_for_wm_program;
 	bool enable_pg_cntl_debug_logs;
+	bool enable_block_sequence_programming;
 };
 
 
