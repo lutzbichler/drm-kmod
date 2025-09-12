d6242b410fdc9fc2b828e6f60fa71b528ef174c4
Jani Nikula <jani.nikula@intel.com>
2025-09-12 17:48:43 +0300

    drm/i915: add vlv_clock_get_czclk()
    Add vlv_clock_get_czclk() helper to avoid looking at i915->czclk_freq
    directly.
    
    Reviewed-by: Mika Kahola <mika.kahola@intel.com>
    Reviewed-by: Michał Grzelak <michal.grzelak@intel.com>
    Acked-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://lore.kernel.org/r/4885f6e486a31c773a3bfebd6936670234e57bd0.1757688216.git.jani.nikula@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/gt/intel_rps.c
+++ drivers/gpu/drm/i915/gt/intel_rps.c
@@ -1783,6 +1783,7 @@ static void vlv_c0_read(struct intel_uncore *uncore, struct intel_rps_ei *ei)
 
 static u32 vlv_wa_c0_ei(struct intel_rps *rps, u32 pm_iir)
 {
+	struct drm_i915_private *i915 = rps_to_i915(rps);
 	struct intel_uncore *uncore = rps_to_uncore(rps);
 	const struct intel_rps_ei *prev = &rps->ei;
 	struct intel_rps_ei now;
@@ -1799,7 +1800,7 @@ static u32 vlv_wa_c0_ei(struct intel_rps *rps, u32 pm_iir)
 
 		time = ktime_us_delta(now.ktime, prev->ktime);
 
-		time *= rps_to_i915(rps)->czclk_freq;
+		time *= vlv_clock_get_czclk(&i915->drm);
 
 		/* Workload can be split between render + media,
 		 * e.g. SwapBuffers being blitted in X after being rendered in
