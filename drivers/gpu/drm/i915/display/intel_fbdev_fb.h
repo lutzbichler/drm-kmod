30fe619245b2da1055f1f732721569cea91fd88d
Jani Nikula <jani.nikula@intel.com>
2025-09-18 11:40:54 +0300

    drm/{i915, xe}/fbdev: deduplicate struct drm_mode_fb_cmd2 init
    [ Upstream commit f9ff39f940f5ddd1d4ffcff602de7206aa1ff05d ]
    
    Pull struct drm_mode_fb_cmd2 initialization out of the driver dependent
    code into shared display code.
    
    v2: Rebase on xe stride alignment change
    
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://lore.kernel.org/r/e922e47bfd39f9c5777f869ff23c23309ebbb380.1758184771.git.jani.nikula@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Stable-dep-of: 460b31720369 ("drm/i915/fbdev: Hold runtime PM ref during fbdev BO creation")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/i915/display/intel_fbdev_fb.h
+++ drivers/gpu/drm/i915/display/intel_fbdev_fb.h
@@ -6,15 +6,19 @@
 #ifndef __INTEL_FBDEV_FB_H__
 #define __INTEL_FBDEV_FB_H__
 
+#ifdef __FreeBSD__
+#define fb_info linux_fb_info
+#endif
+
 struct drm_device;
-struct drm_fb_helper_surface_size;
 struct drm_gem_object;
+struct drm_mode_fb_cmd2;
 struct fb_info;
 struct i915_vma;
 struct intel_display;
 
 struct intel_framebuffer *intel_fbdev_fb_alloc(struct drm_device *drm,
-					       struct drm_fb_helper_surface_size *sizes);
+					       struct drm_mode_fb_cmd2 *mode_cmd);
 int intel_fbdev_fb_fill_info(struct intel_display *display, struct fb_info *info,
 			     struct drm_gem_object *obj, struct i915_vma *vma);
 
