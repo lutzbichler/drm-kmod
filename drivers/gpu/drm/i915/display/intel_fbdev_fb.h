19afd95d1bd58827539fce53e0f61b5b2c0a565a
Jani Nikula <jani.nikula@intel.com>
2025-09-18 11:40:55 +0300

    drm/i915/fbdev: abstract bo creation MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    Separate fbdev bo creation into a separate function
    intel_fbdev_fb_bo_create().
    
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://lore.kernel.org/r/cb3999ceae43d56e075c28a1f4581169ce457ab0.1758184771.git.jani.nikula@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_fbdev_fb.h
+++ drivers/gpu/drm/i915/display/intel_fbdev_fb.h
@@ -17,6 +17,7 @@ struct intel_display;
 #define	fb_info	linux_fb_info
 #endif
 
+struct drm_gem_object *intel_fbdev_fb_bo_create(struct drm_device *drm, int size);
 struct intel_framebuffer *intel_fbdev_fb_alloc(struct drm_device *drm,
 					       struct drm_mode_fb_cmd2 *mode_cmd);
 int intel_fbdev_fb_fill_info(struct intel_display *display, struct fb_info *info,
