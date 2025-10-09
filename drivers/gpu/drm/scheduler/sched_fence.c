13fdba7ecbdbbdcc439b19e2623a09d9e47da102
Christian König <christian.koenig@amd.com>
2025-10-09 10:40:06 +0200

    dma-buf: abstract fence locking v2 MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    Add dma_fence_lock_irqsafe() and dma_fence_unlock_irqrestore() wrappers
    and mechanically apply them everywhere.
    
    Just a pre-requisite cleanup for a follow up patch.
    
    v2: add some missing i915 bits, add abstraction for lockdep assertion as
        well
    v3: one more suggestion by Tvrtko
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Link: https://lore.kernel.org/r/20260219160822.1529-4-christian.koenig@amd.com

--- drivers/gpu/drm/scheduler/sched_fence.c
+++ drivers/gpu/drm/scheduler/sched_fence.c
@@ -156,19 +156,19 @@ static void drm_sched_fence_set_deadline_finished(struct dma_fence *f,
 	struct dma_fence *parent;
 	unsigned long flags;
 
-	spin_lock_irqsave(&fence->lock, flags);
+	dma_fence_lock_irqsave(f, flags);
 
 	/* If we already have an earlier deadline, keep it: */
 	if (test_bit(DRM_SCHED_FENCE_FLAG_HAS_DEADLINE_BIT, &f->flags) &&
 	    ktime_before(fence->deadline, deadline)) {
-		spin_unlock_irqrestore(&fence->lock, flags);
+		dma_fence_unlock_irqrestore(f, flags);
 		return;
 	}
 
 	fence->deadline = deadline;
 	set_bit(DRM_SCHED_FENCE_FLAG_HAS_DEADLINE_BIT, &f->flags);
 
-	spin_unlock_irqrestore(&fence->lock, flags);
+	dma_fence_unlock_irqrestore(f, flags);
 
 	/*
 	 * smp_load_aquire() to ensure that if we are racing another
