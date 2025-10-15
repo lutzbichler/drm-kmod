113acc2d3c056fa359e5d89a785bad6de01ed0e4
Matthew Brost <matthew.brost@intel.com>
2025-10-15 14:03:20 +0200

    drm/gpusvm, drm/xe: Allow mixed mappings for userptr
    Compute kernels often issue memory copies immediately after completion.
    If the memory being copied is an SVM pointer that was faulted into the
    device and then bound via userptr, it is undesirable to move that
    memory. Worse, if userptr is mixed between system and device memory, the
    bind operation may be rejected.
    
    Xe already has the necessary plumbing to support userptr with mixed
    mappings. This update modifies GPUSVM's get_pages to correctly locate
    pages in such mixed mapping scenarios.
    
    v2:
    - Rebase (Thomas Hellström)
    v3:
    - Remove Fixes tag.
    v4:
    - Break out from series since the other patch was merged.
    - Update patch subject, ensure dri-devel and Maarten are CC'd.
    
    Cc: Maarten Lankhorst <maarten.lankhorst@intel.com>
    Cc: dri-devel@lists.freedesktop.org
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>
    Reviewed-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Reviewed-by: Himal Prasad Ghimiray <himal.prasad.ghimiray@intel.com>
    Link: https://lore.kernel.org/r/20251015120320.176338-1-thomas.hellstrom@linux.intel.com
    Acked-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>

--- drivers/gpu/drm/xe/xe_userptr.c
+++ drivers/gpu/drm/xe/xe_userptr.c
@@ -3,6 +3,7 @@
  * Copyright © 2025 Intel Corporation
  */
 
+#include "xe_svm.h"
 #include "xe_userptr.h"
 
 #include <linux/mm.h>
@@ -58,7 +59,8 @@ int xe_vma_userptr_pin_pages(struct xe_userptr_vma *uvma)
 	struct xe_device *xe = vm->xe;
 	struct drm_gpusvm_ctx ctx = {
 		.read_only = xe_vma_read_only(vma),
-		.device_private_page_owner = NULL,
+		.device_private_page_owner = xe_svm_devm_owner(xe),
+		.allow_mixed = true,
 	};
 
 	lockdep_assert_held(&vm->lock);
