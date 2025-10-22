9e9e9bd3c9ec5c3086caf04434651a4faa930d34
Jani Nikula <jani.nikula@intel.com>
2025-10-22 19:10:54 +0300

    drm/{i915,xe}/fbdev: add intel_fbdev_fb_pitch_align() MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    For reasons still unknown, xe appears to require a stride alignment of
    XE_PAGE_SIZE, and using 64 leads to sporadic failures. Go back to having
    separate stride alignment for i915 and xe, until the issue is root
    caused.
    
    v2: Add FIXME comment, reference issue with Link (Ville)
    
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Jouni Högander <jouni.hogander@intel.com>
    Cc: Maarten Lankhorst <maarten@lankhorst.se>
    Link: https://gitlab.freedesktop.org/drm/xe/kernel/-/issues/6220
    Fixes: 4a36b339a14a ("drm/xe/fbdev: use the same 64-byte stride alignment as i915")
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://lore.kernel.org/r/ae51d1e224048bdc87bf7a56d8f5ebd0fbb6a383.1756931441.git.jani.nikula@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://lore.kernel.org/r/20251022161054.708388-1-jani.nikula@intel.com

--- drivers/gpu/drm/i915/display/intel_fbdev_fb.h
+++ drivers/gpu/drm/i915/display/intel_fbdev_fb.h
@@ -6,17 +6,19 @@
 #ifndef __INTEL_FBDEV_FB_H__
 #define __INTEL_FBDEV_FB_H__
 
+#include <linux/types.h>
+
 struct drm_device;
 struct drm_gem_object;
 struct drm_mode_fb_cmd2;
 struct fb_info;
 struct i915_vma;
-struct intel_display;
 
 #ifdef __FreeBSD__
 #define	fb_info	linux_fb_info
 #endif
 
+u32 intel_fbdev_fb_pitch_align(u32 stride);
 struct drm_gem_object *intel_fbdev_fb_bo_create(struct drm_device *drm, int size);
 void intel_fbdev_fb_bo_destroy(struct drm_gem_object *obj);
 int intel_fbdev_fb_fill_info(struct drm_device *drm, struct fb_info *info,
