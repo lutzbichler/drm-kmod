d49af2af5c842536cef768fc45111d8fec48ebb3
Lutz Bichler <Lutz.Bichler@gmail.com>
2025-10-30 14:44:32 +0100

    drm/amd: Final sync with Linux v6.17

--- drivers/gpu/drm/amd/display/dc/dc_types.h
+++ drivers/gpu/drm/amd/display/dc/dc_types.h
@@ -1257,7 +1257,6 @@ enum dc_cm2_gpu_mem_layout {
 
 enum dc_cm2_gpu_mem_pixel_component_order {
 	DC_CM2_GPU_MEM_PIXEL_COMPONENT_ORDER_RGBA,
-	DC_CM2_GPU_MEM_PIXEL_COMPONENT_ORDER_BGRA
 };
 
 enum dc_cm2_gpu_mem_format {
@@ -1279,7 +1278,6 @@ struct dc_cm2_gpu_mem_format_parameters {
 
 enum dc_cm2_gpu_mem_size {
 	DC_CM2_GPU_MEM_SIZE_171717,
-	DC_CM2_GPU_MEM_SIZE_333333,
 	DC_CM2_GPU_MEM_SIZE_TRANSFORMED,
 };
 
