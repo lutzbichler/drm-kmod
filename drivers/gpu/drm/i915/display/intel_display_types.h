64842cfc2d6b7e66f80aa74700fc7d37b81772c8
Jani Nikula <jani.nikula@intel.com>
2025-11-25 15:24:42 +0200

    drm/i915/power: convert intel_wakeref_t to struct ref_tracker *
    Under the hood, intel_wakeref_t is just struct ref_tracker *. Use the
    actual underlying type both for clarity (we *are* using intel_wakeref_t
    as a pointer though it doesn't look like one) and to help i915, xe and
    display coexistence without custom types.
    
    v2: Keep intel_wakeref.h includes as they are
    
    Reviewed-by: Luca Coelho <luciano.coelho@intel.com>
    Link: https://patch.msgid.link/f182bd26d5f9a00e843246d4aac8b25ff7531c51.1764076995.git.jani.nikula@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_types.h
+++ drivers/gpu/drm/i915/display/intel_display_types.h
@@ -1667,7 +1667,7 @@ struct intel_pps {
 	unsigned long last_power_on;
 	unsigned long last_backlight_off;
 	ktime_t panel_power_off_time;
-	intel_wakeref_t vdd_wakeref;
+	struct ref_tracker *vdd_wakeref;
 
 	union {
 		/*
@@ -1949,8 +1949,8 @@ struct intel_digital_port {
 	/* Used for DP and ICL+ TypeC/DP and TypeC/HDMI ports. */
 	enum aux_ch aux_ch;
 	enum intel_display_power_domain ddi_io_power_domain;
-	intel_wakeref_t ddi_io_wakeref;
-	intel_wakeref_t aux_wakeref;
+	struct ref_tracker *ddi_io_wakeref;
+	struct ref_tracker *aux_wakeref;
 
 	struct intel_tc_port *tc;
 
--- drivers/gpu/drm/xe/compat-i915-headers/intel_wakeref.h
+++ drivers/gpu/drm/xe/compat-i915-headers/intel_wakeref.h
@@ -6,10 +6,6 @@
 #ifndef COMPAT_I915_HEADER_INTEL_WAKEREF_
 #define COMPAT_I915_HEADER_INTEL_WAKEREF_
 
-#include <linux/types.h>
-
-typedef struct ref_tracker *intel_wakeref_t;
-
 #define INTEL_WAKEREF_DEF ERR_PTR(-ENOENT)
 
 #endif /* COMPAT_I915_HEADER_INTEL_WAKEREF_ */
