44627a8b8a2580d00101bd02e0fa5685454efa9e
Lijo Lazar <lijo.lazar@amd.com>
2025-11-25 18:03:30 +0530

    drm/amd/pm: Use generic pcie dpm table for SMUv14
    Use smu_pcie_table for SMUv14 SOCs.
    
    Signed-off-by: Lijo Lazar <lijo.lazar@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/swsmu/inc/smu_v14_0.h
+++ drivers/gpu/drm/amd/pm/swsmu/inc/smu_v14_0.h
@@ -67,13 +67,6 @@ struct smu_14_0_max_sustainable_clocks {
 	uint32_t soc_clock;
 };
 
-struct smu_14_0_pcie_table {
-	uint8_t  pcie_gen[MAX_PCIE_CONF];
-	uint8_t  pcie_lane[MAX_PCIE_CONF];
-	uint16_t clk_freq[MAX_PCIE_CONF];
-	uint32_t num_of_link_levels;
-};
-
 struct smu_14_0_dpm_tables {
 	struct smu_dpm_table        soc_table;
 	struct smu_dpm_table        gfx_table;
@@ -86,7 +79,7 @@ struct smu_14_0_dpm_tables {
 	struct smu_dpm_table        display_table;
 	struct smu_dpm_table        phy_table;
 	struct smu_dpm_table        fclk_table;
-	struct smu_14_0_pcie_table       pcie_table;
+	struct smu_pcie_table       pcie_table;
 };
 
 struct smu_14_0_dpm_context {
