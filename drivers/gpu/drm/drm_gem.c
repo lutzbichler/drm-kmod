2f9d3dbb2391d5dcbd45f1f6e1abdd324930b079
Loïc Molinari <loic.molinari@collabora.com>
2025-12-05 19:22:28 +0100

    drm/gem: Get rid of *_with_mnt helpers MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    drm_gem_object_init_with_mnt() and drm_gem_shmem_create_with_mnt() can
    be removed now that the drivers use the new drm_gem_huge_mnt_create()
    and drm_gem_get_huge_mnt() helpers.
    
    v5:
    - use drm_gem_has_huge_mnt() helper
    - compile out shmem_file_setup_with_mnt() call in builds with
      CONFIG_TRANSPARENT_HUGEPAGE=n
    
    v9:
    - replace drm_gem_has_huge_mnt() with drm_gem_get_huge_mnt()
    
    Signed-off-by: Loïc Molinari <loic.molinari@collabora.com>
    Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
    Reviewed-by: Maíra Canal <mcanal@igalia.com>
    Link: https://patch.msgid.link/20251205182231.194072-8-loic.molinari@collabora.com
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

--- drivers/gpu/drm/drm_gem.c
+++ drivers/gpu/drm/drm_gem.c
@@ -175,36 +175,34 @@ drm_gem_init(struct drm_device *dev)
 }
 
 /**
- * drm_gem_object_init_with_mnt - initialize an allocated shmem-backed GEM
+ * drm_gem_object_init - initialize an allocated shmem-backed GEM object
  * object in a given shmfs mountpoint
  *
  * @dev: drm_device the object should be initialized for
  * @obj: drm_gem_object to initialize
  * @size: object size
- * @gemfs: tmpfs mount where the GEM object will be created. If NULL, use
- * the usual tmpfs mountpoint (`shm_mnt`).
  *
  * Initialize an already allocated GEM object of the specified size with
- * shmfs backing store.
+ * shmfs backing store. A huge mountpoint can be used by calling
+ * drm_gem_huge_mnt_create() beforehand.
  */
-int drm_gem_object_init_with_mnt(struct drm_device *dev,
-				 struct drm_gem_object *obj, size_t size,
-#ifdef __linux__
-				 struct vfsmount *gemfs)
-#elif defined(__FreeBSD__)
-				 void *gemfs)
-#endif
-{
+int drm_gem_object_init(struct drm_device *dev, struct drm_gem_object *obj,
+			size_t size)
+ {
+	struct vfsmount *huge_mnt;
 	struct file *filp;
 
 	drm_gem_private_object_init(dev, obj, size);
 
+	huge_mnt = drm_gem_get_huge_mnt(dev);
+	if (huge_mnt)
 #ifdef __linux__
-	if (gemfs)
-		filp = shmem_file_setup_with_mnt(gemfs, "drm mm object", size,
-						 VM_NORESERVE);
-	else
+		filp = shmem_file_setup_with_mnt(huge_mnt, "drm mm object",
+						 size, VM_NORESERVE);
+#elif defined(__FreeBSD__)
+		panic("drm_gem.c: huge mnt not supported\n");
 #endif
+	else
 		filp = shmem_file_setup("drm mm object", size, VM_NORESERVE);
 
 	if (IS_ERR(filp))
@@ -213,22 +211,6 @@ int drm_gem_object_init_with_mnt(struct drm_device *dev,
 	obj->filp = filp;
 
 	return 0;
-}
-EXPORT_SYMBOL(drm_gem_object_init_with_mnt);
-
-/**
- * drm_gem_object_init - initialize an allocated shmem-backed GEM object
- * @dev: drm_device the object should be initialized for
- * @obj: drm_gem_object to initialize
- * @size: object size
- *
- * Initialize an already allocated GEM object of the specified size with
- * shmfs backing store.
- */
-int drm_gem_object_init(struct drm_device *dev, struct drm_gem_object *obj,
-			size_t size)
-{
-	return drm_gem_object_init_with_mnt(dev, obj, size, NULL);
 }
 EXPORT_SYMBOL(drm_gem_object_init);
 
