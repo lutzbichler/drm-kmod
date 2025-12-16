81c12998416f0c322bcc52e4017ec951c09c5e24
Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>
2025-12-16 10:07:53 +0200

    drm/i915: switch to use kernel standard error injection
    Switch error injection testing from i915_inject_probe_failure
    to ALLOW_ERROR_INJECTION. Here taken out calls to
    i915_inject_probe_failure and changed to use
    ALLOW_ERROR_INJECTION for the same functions.
    
    Below functions are dropped from testing since I
    couldn't hit those at module bind time, testing
    these would just fail the tests. To include these
    in test would need to find way to cause resetting in i915
    which would trigger these:
    
    intel_gvt_init
    intel_wopcm_init
    intel_uc_fw_upload
    intel_gt_init with expected -EIO (-EINVAL is tested)
    lrc_init_wa_ctx
    intel_huc_auth
    guc_check_version_range
    intel_uc_fw_fetch
    uc_fw_xfer
    __intel_uc_reset_hw
    guc_enable_communication
    uc_init_wopcm
    ..and all stages of __force_fw_fetch_failures
    
    Signed-off-by: Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Mika Kahola <mika.kahola@intel.com>
    Link: https://patch.msgid.link/20251216080754.221974-2-juhapekka.heikkila@gmail.com

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -1006,11 +1006,6 @@ static int i915_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	if (err)
 		return err;
 
-	if (i915_inject_probe_failure(pdev_to_i915(pdev))) {
-		i915_pci_remove(pdev);
-		return -ENODEV;
-	}
-
 	err = i915_live_selftests(pdev);
 	if (err) {
 		i915_pci_remove(pdev);
@@ -1025,6 +1020,7 @@ static int i915_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	return 0;
 }
+ALLOW_ERROR_INJECTION(i915_pci_probe, ERRNO);
 
 static void i915_pci_shutdown(struct pci_dev *pdev)
 {
