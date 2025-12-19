957c39693587cddf792fa2187346131661315b85
Thomas Hellström <thomas.hellstrom@linux.intel.com>
2025-12-19 12:33:04 +0100

    drm/xe: Use the drm_pagemap cache and shrinker MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    Define a struct xe_pagemap that embeds all pagemap-related
    data used by xekmd, and use the drm_pagemap cache- and
    shrinker to manage lifetime.
    
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Reviewed-by: Matthew Brost <matthew.brost@intel.com>
    Link: https://patch.msgid.link/20251219113320.183860-9-thomas.hellstrom@linux.intel.com

--- drivers/gpu/drm/xe/xe_device_types.h
+++ drivers/gpu/drm/xe/xe_device_types.h
@@ -36,6 +36,7 @@
 #define TEST_VM_OPS_ERROR
 #endif
 
+struct drm_pagemap_shrinker;
 struct intel_display;
 struct intel_dg_nvm_dev;
 struct xe_ggtt;
@@ -465,6 +466,10 @@ struct xe_device {
 #define XE_PAGEFAULT_QUEUE_COUNT	4
 		/** @usm.pf_queue: Page fault queues */
 		struct xe_pagefault_queue pf_queue[XE_PAGEFAULT_QUEUE_COUNT];
+#if IS_ENABLED(CONFIG_DRM_XE_PAGEMAP)
+		/** @usm.pagemap_shrinker: Shrinker for unused pagemaps */
+		struct drm_pagemap_shrinker *dpagemap_shrinker;
+#endif
 	} usm;
 
 	/** @pinned: pinned BO state */
