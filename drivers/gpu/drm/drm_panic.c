dcab8b95f56e4375306ad210c3b399a3766e10c7
Nathan Chancellor <nathan@kernel.org>
2026-01-05 23:19:27 -0700

    drm/panic: Fix expected string for QR_CODE in drm_panic_type_map
    The help text of CONFIG_DRM_PANIC_SCREEN_QR_CODE documents the expected
    value to CONFIG_DRM_PANIC_SCREEN as "qr_code" but drm_panic_type_map
    checks for "qr". Adjust drm_panic_type_map and the module description to
    match so that existing configurations do not stop working.
    
    Fixes: e85e9ccf3f84 ("drm/panic: Report invalid or unsupported panic modes")
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Reviewed-by: Jocelyn Falempe <jfalempe@redhat.com>
    Signed-off-by: Tvrtko Ursulin <tursulin@ursulin.net>
    Link: https://lore.kernel.org/r/20260105-drm_panic-handle-invalid-drm_panic_screen-v1-2-55228bd4b0f8@kernel.org

--- drivers/gpu/drm/drm_panic.c
+++ drivers/gpu/drm/drm_panic.c
@@ -823,7 +823,7 @@ static const char *drm_panic_type_map[] = {
 	[DRM_PANIC_TYPE_KMSG] = "kmsg",
 	[DRM_PANIC_TYPE_USER] = "user",
 #if IS_ENABLED(CONFIG_DRM_PANIC_SCREEN_QR_CODE)
-	[DRM_PANIC_TYPE_QR] = "qr",
+	[DRM_PANIC_TYPE_QR] = "qr_code",
 #endif
 };
 
@@ -858,7 +858,7 @@ module_param_cb(panic_screen, &drm_panic_ops, NULL, 0644);
 
 MODULE_PARM_DESC(panic_screen,
 #if IS_ENABLED(CONFIG_DRM_PANIC_SCREEN_QR_CODE)
-		 "Choose what will be displayed by drm_panic, 'user', 'kmsg' or 'qr' [default="
+		 "Choose what will be displayed by drm_panic, 'user', 'kmsg' or 'qr_code' [default="
 #else
 		 "Choose what will be displayed by drm_panic, 'user' or 'kmsg' [default="
 #endif
