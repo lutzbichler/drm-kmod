3a2894d91cedbf747c7721249094aa2a3cdb2313
Nitin Gote <nitin.r.gote@intel.com>
2026-01-20 11:17:25 +0530

    drm/xe: derive mem copy capability from graphics version
    Drop .has_mem_copy_instr from the platform descriptors and set it
    in xe_info_init() after handle_gmdid() populates graphics_verx100.
    Centralizing the GRAPHICS_VER(xe) >= 20 check keeps MEM_COPY enabled
    on Xe2+ and removes redundant per-platform plumbing.
    
    Bspec: 57561
    
    Fixes: 1e12dbae9d72 ("drm/xe/migrate: support MEM_COPY instruction")
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Suggested-by: Matthew Auld <matthew.auld@intel.com>
    Signed-off-by: Nitin Gote <nitin.r.gote@intel.com>
    Link: https://patch.msgid.link/20260120054724.1982608-2-nitin.r.gote@intel.com
    Signed-off-by: Tejas Upadhyay <tejas.upadhyay@intel.com>
    (cherry picked from commit 6ef02656c3222b1e12032a40d644ed56806b14fc)
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>

--- drivers/gpu/drm/xe/xe_pci.c
+++ drivers/gpu/drm/xe/xe_pci.c
@@ -346,7 +346,6 @@ static const struct xe_device_desc lnl_desc = {
 	.has_display = true,
 	.has_flat_ccs = 1,
 	.has_pxp = true,
-	.has_mem_copy_instr = true,
 	.max_gt_per_tile = 2,
 	.needs_scratch = true,
 	.va_bits = 48,
@@ -367,7 +366,6 @@ static const struct xe_device_desc bmg_desc = {
 	.has_heci_cscfi = 1,
 	.has_late_bind = true,
 	.has_sriov = true,
-	.has_mem_copy_instr = true,
 	.max_gt_per_tile = 2,
 	.needs_scratch = true,
 	.subplatforms = (const struct xe_subplatform_desc[]) {
@@ -384,7 +382,6 @@ static const struct xe_device_desc ptl_desc = {
 	.has_display = true,
 	.has_flat_ccs = 1,
 	.has_sriov = true,
-	.has_mem_copy_instr = true,
 	.max_gt_per_tile = 2,
 	.needs_scratch = true,
 	.needs_shared_vf_gt_wq = true,
@@ -397,7 +394,6 @@ static const struct xe_device_desc nvls_desc = {
 	.dma_mask_size = 46,
 	.has_display = true,
 	.has_flat_ccs = 1,
-	.has_mem_copy_instr = true,
 	.max_gt_per_tile = 2,
 	.require_force_probe = true,
 	.va_bits = 48,
@@ -679,7 +675,6 @@ static int xe_info_init_early(struct xe_device *xe,
 	xe->info.has_pxp = desc->has_pxp;
 	xe->info.has_sriov = xe_configfs_primary_gt_allowed(to_pci_dev(xe->drm.dev)) &&
 		desc->has_sriov;
-	xe->info.has_mem_copy_instr = desc->has_mem_copy_instr;
 	xe->info.skip_guc_pc = desc->skip_guc_pc;
 	xe->info.skip_mtcfg = desc->skip_mtcfg;
 	xe->info.skip_pcode = desc->skip_pcode;
@@ -868,6 +863,7 @@ static int xe_info_init(struct xe_device *xe,
 	xe->info.has_range_tlb_inval = graphics_desc->has_range_tlb_inval;
 	xe->info.has_usm = graphics_desc->has_usm;
 	xe->info.has_64bit_timestamp = graphics_desc->has_64bit_timestamp;
+	xe->info.has_mem_copy_instr = GRAPHICS_VER(xe) >= 20;
 
 	xe_info_probe_tile_count(xe);
 
