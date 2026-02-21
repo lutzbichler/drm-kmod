3f71bb36fe437c850b1c37a9221798efc20d7edb
Kees Cook <kees@kernel.org>
2026-02-20 23:49:23 -0800

    treewide: Replace kmalloc with kmalloc_obj for non-scalar types
    This is the result of running the Coccinelle script from
    scripts/coccinelle/api/kmalloc_objs.cocci. The script is designed to
    avoid scalar types (which need careful case-by-case checking), and
    instead replace kmalloc-family calls that allocate struct or union
    object instances:
    
    Single allocations:     kmalloc(sizeof(TYPE), ...)
    are replaced with:      kmalloc_obj(TYPE, ...)
    
    Array allocations:      kmalloc_array(COUNT, sizeof(TYPE), ...)
    are replaced with:      kmalloc_objs(TYPE, COUNT, ...)
    
    Flex array allocations: kmalloc(struct_size(PTR, FAM, COUNT), ...)
    are replaced with:      kmalloc_flex(*PTR, FAM, COUNT, ...)
    
    (where TYPE may also be *VAR)
    
    The resulting allocations no longer return "void *", instead returning
    "TYPE *".
    
    Signed-off-by: Kees Cook <kees@kernel.org>

--- drivers/gpu/drm/i915/gt/intel_execlists_submission.c
+++ drivers/gpu/drm/i915/gt/intel_execlists_submission.c
@@ -2260,7 +2260,7 @@ static struct execlists_capture *capture_regs(struct intel_engine_cs *engine)
 	const gfp_t gfp = GFP_ATOMIC | __GFP_NOWARN;
 	struct execlists_capture *cap;
 
-	cap = kmalloc(sizeof(*cap), gfp);
+	cap = kmalloc_obj(*cap, gfp);
 	if (!cap)
 		return NULL;
 
@@ -3940,7 +3940,7 @@ execlists_create_virtual(struct intel_engine_cs **siblings, unsigned int count,
 	unsigned int n;
 	int err;
 
-	ve = kzalloc(struct_size(ve, siblings, count), GFP_KERNEL);
+	ve = kzalloc_flex(*ve, siblings, count, GFP_KERNEL);
 	if (!ve)
 		return ERR_PTR(-ENOMEM);
 
