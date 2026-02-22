47937049963e7a2edf9d2fea314e4f8b2b4a9b08
Linus Torvalds <torvalds@linux-foundation.org>
2026-02-21 20:03:00 -0800

    Convert more 'alloc_obj' cases to default GFP_KERNEL arguments
    This converts some of the visually simpler cases that have been split
    over multiple lines.  I only did the ones that are easy to verify the
    resulting diff by having just that final GFP_KERNEL argument on the next
    line.
    
    Somebody should probably do a proper coccinelle script for this, but for
    me the trivial script actually resulted in an assertion failure in the
    middle of the script.  I probably had made it a bit _too_ trivial.
    
    So after fighting that far a while I decided to just do some of the
    syntactically simpler cases with variations of the previous 'sed'
    scripts.
    
    The more syntactically complex multi-line cases would mostly really want
    whitespace cleanup anyway.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/gpu/drm/radeon/atombios_encoders.c
+++ drivers/gpu/drm/radeon/atombios_encoders.c
@@ -2627,8 +2627,7 @@ radeon_atombios_set_dac_info(struct radeon_encoder *radeon_encoder)
 {
 	struct drm_device *dev = radeon_encoder->base.dev;
 	struct radeon_device *rdev = dev->dev_private;
-	struct radeon_encoder_atom_dac *dac = kzalloc_obj(struct radeon_encoder_atom_dac,
-							  GFP_KERNEL);
+	struct radeon_encoder_atom_dac *dac = kzalloc_obj(struct radeon_encoder_atom_dac);
 
 	if (!dac)
 		return NULL;
@@ -2641,8 +2640,7 @@ static struct radeon_encoder_atom_dig *
 radeon_atombios_set_dig_info(struct radeon_encoder *radeon_encoder)
 {
 	int encoder_enum = (radeon_encoder->encoder_enum & ENUM_ID_MASK) >> ENUM_ID_SHIFT;
-	struct radeon_encoder_atom_dig *dig = kzalloc_obj(struct radeon_encoder_atom_dig,
-							  GFP_KERNEL);
+	struct radeon_encoder_atom_dig *dig = kzalloc_obj(struct radeon_encoder_atom_dig);
 
 	if (!dig)
 		return NULL;
