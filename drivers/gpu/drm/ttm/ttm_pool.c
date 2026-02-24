da3e7c83dd8ec78cc0657142e1c55135e7faa17a
Dave Airlie <airlied@redhat.com>
2026-02-24 12:06:21 +1000

    ttm/pool: drop numa specific pools
    The list_lru will now handle numa for us, so no need to keep
    separate pool types for it. Just consolidate into the global ones.
    
    This adds a debugfs change to avoid dumping non-existant orders due
    to this change.
    
    Cc: Christian Koenig <christian.koenig@amd.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/ttm/ttm_pool.c
+++ drivers/gpu/drm/ttm/ttm_pool.c
@@ -484,16 +484,20 @@ static struct ttm_pool_type *ttm_pool_select_type(struct ttm_pool *pool,
 #ifdef CONFIG_X86
 	switch (caching) {
 	case ttm_write_combined:
+#if defined(__FreeBSD__) && !defined(PAGE_IS_LKPI_PAGE)
 		if (pool->nid != NUMA_NO_NODE)
 			return &pool->caching[caching].orders[order];
+#endif
 
 		if (ttm_pool_uses_dma32(pool))
 			return &global_dma32_write_combined[order];
 
 		return &global_write_combined[order];
 	case ttm_uncached:
+#if defined(__FreeBSD__) && !defined(PAGE_IS_LKPI_PAGE)
 		if (pool->nid != NUMA_NO_NODE)
 			return &pool->caching[caching].orders[order];
+#endif
 
 		if (ttm_pool_uses_dma32(pool))
 			return &global_dma32_uncached[order];
@@ -1452,7 +1456,7 @@ int ttm_pool_debugfs(struct ttm_pool *pool, struct seq_file *m)
 {
 	unsigned int i;
 
-	if (!ttm_pool_uses_dma_alloc(pool) && pool->nid == NUMA_NO_NODE) {
+	if (!ttm_pool_uses_dma_alloc(pool)) {
 		seq_puts(m, "unused\n");
 		return 0;
 	}
@@ -1463,10 +1467,7 @@ int ttm_pool_debugfs(struct ttm_pool *pool, struct seq_file *m)
 	for (i = 0; i < TTM_NUM_CACHING_TYPES; ++i) {
 		if (!ttm_pool_select_type(pool, i, 0))
 			continue;
-		if (ttm_pool_uses_dma_alloc(pool))
-			seq_puts(m, "DMA ");
-		else
-			seq_printf(m, "N%d ", pool->nid);
+		seq_puts(m, "DMA ");
 		switch (i) {
 		case ttm_cached:
 			seq_puts(m, "\t:");
