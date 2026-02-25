b8df92b4a1c1b30bfb1c07b3b42e1644ae5dc3f5
Himal Prasad Ghimiray <himal.prasad.ghimiray@intel.com>
2026-02-25 22:17:48 +0530

    drm/xe/guc: Skip access counter queue init for unsupported platforms
    Add a has_access_counter feature flag to the graphics IP descriptor
    and skip writing parameters for the access counter queue in
    guc_um_init_params(), leaving queue_params[2] zero-initialized
    to signal unavailability to the GuC.
    
    The queue_params[] array layout is fixed by firmware ABI, so we
    maintain the structure with queues 0 and 1 (page fault request/response)
    always configured, and queue 2 conditionally skipped based on the
    has_access_counter flag.
    
    Bspec: 59323
    Cc: Stuart Summers <stuart.summers@intel.com>
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Suggested-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Signed-off-by: Himal Prasad Ghimiray <himal.prasad.ghimiray@intel.com>
    Signed-off-by: Varun Gupta <varun.gupta1@intel.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    Link: https://patch.msgid.link/20260225164748.2302380-1-varun.gupta@intel.com
    Signed-off-by: Tejas Upadhyay <tejas.upadhyay@intel.com>

--- drivers/gpu/drm/xe/xe_pci.c
+++ drivers/gpu/drm/xe/xe_pci.c
@@ -85,6 +85,7 @@ static const struct xe_graphics_desc graphics_xehpc = {
 
 	XE_HP_FEATURES,
 
+	.has_access_counter = 1,
 	.has_asid = 1,
 	.has_atomic_enable_pte_bit = 1,
 	.has_usm = 1,
@@ -102,6 +103,7 @@ static const struct xe_graphics_desc graphics_xelpg = {
 };
 
 #define XE2_GFX_FEATURES \
+	.has_access_counter = 1, \
 	.has_asid = 1, \
 	.has_atomic_enable_pte_bit = 1, \
 	.has_range_tlb_inval = 1, \
@@ -127,6 +129,7 @@ static const struct xe_graphics_desc graphics_xe3p_lpg = {
 
 static const struct xe_graphics_desc graphics_xe3p_xpc = {
 	XE2_GFX_FEATURES,
+	.has_access_counter = 0,
 	.has_indirect_ring_state = 1,
 	.hw_engine_mask =
 		GENMASK(XE_HW_ENGINE_BCS8, XE_HW_ENGINE_BCS1) |
@@ -948,6 +951,7 @@ static int xe_info_init(struct xe_device *xe,
 		media_desc = NULL;
 	}
 
+	xe->info.has_access_counter = graphics_desc->has_access_counter;
 	xe->info.has_asid = graphics_desc->has_asid;
 	xe->info.has_atomic_enable_pte_bit = graphics_desc->has_atomic_enable_pte_bit;
 	if (xe->info.platform != XE_PVC)
