f4e85eaf4ee75c0980476a41cf804a1eeab8a716
Michal Wajdeczko <michal.wajdeczko@intel.com>
2026-05-12 20:33:39 +0200

    drm/xe/display: Add macro with display driver ops
    Instead of updating the drm_driver.fbdev_probe field in the runtime,
    we can use macro which value depends on the actual Kconfig setup.
    
    The .fbdev_probe hook will not be used by the drm core unless we
    also enable a DRIVER_MODESET driver feature flag, and this flag
    still depends on the xe_modparam.probe_display parameter.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patch.msgid.link/20260512183342.3374-5-michal.wajdeczko@intel.com

--- drivers/gpu/drm/xe/xe_device.c
+++ drivers/gpu/drm/xe/xe_device.c
@@ -418,6 +418,7 @@ static struct drm_driver regular_driver = {
 	.major = DRIVER_MAJOR,
 	.minor = DRIVER_MINOR,
 	.patchlevel = DRIVER_PATCHLEVEL,
+	XE_DISPLAY_DRIVER_OPS,
 };
 
 #ifdef CONFIG_PCI_IOV
@@ -439,6 +440,7 @@ static struct drm_driver admin_only_driver = {
 	.major = DRIVER_MAJOR,
 	.minor = DRIVER_MINOR,
 	.patchlevel = DRIVER_PATCHLEVEL,
+	XE_DISPLAY_DRIVER_OPS,
 };
 
 /**
