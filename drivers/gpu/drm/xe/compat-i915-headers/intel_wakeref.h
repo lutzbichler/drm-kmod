3a663bbc0340bbecb3bc96a1e9aa91bdfd44aa9a
Lutz Bichler <Lutz.Bichler@gmail.com>
2026-05-15 22:57:25 +0200

    drm/xe: Initial import
    - only briefly tested on i7 1365U
    - contains a workaround to avoid firmware overwriting
      (no idea yet, why this a problem here but is not on Linux)

---
+++ drivers/gpu/drm/xe/compat-i915-headers/intel_wakeref.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2023 Intel Corporation
+ */
+
+#ifndef COMPAT_I915_HEADER_INTEL_WAKEREF_
+#define COMPAT_I915_HEADER_INTEL_WAKEREF_
+
+#include <linux/types.h>
+
+typedef unsigned long intel_wakeref_t;
+
+#endif /* COMPAT_I915_HEADER_INTEL_WAKEREF_ */
+
