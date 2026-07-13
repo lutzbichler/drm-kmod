d58e3e3ee4b6a1af6314f4513febdf32f5060a85
Linmao Li <lilinmao@kylinos.cn>
2026-07-13 16:30:53 +0800

    drm/xe/oa: Check managed mutex initialization errors MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    drmm_mutex_init() can fail while registering its managed cleanup action.
    On failure, the reset path destroys the mutex, so continuing OA setup
    leaves an unusable lock that later paths may acquire.
    
    Return the error from per-GT OA initialization and abort device-wide OA
    initialization if the metrics lock cannot be initialized.
    
    Fixes: a9f905ae7b6f ("drm/xe/oa/uapi: Initialize OA units")
    Fixes: cdf02fe1a94a ("drm/xe/oa/uapi: Add/remove OA config perf ops")
    Signed-off-by: Linmao Li <lilinmao@kylinos.cn>
    Reviewed-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Signed-off-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Link: https://patch.msgid.link/20260713083053.321091-1-lilinmao@kylinos.cn
    (cherry picked from commit 360b293de27bfdd0d07047f8efd5ba8e91fa90b7)
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>

--- drivers/gpu/drm/xe/xe_oa.c
+++ drivers/gpu/drm/xe/xe_oa.c
@@ -2725,9 +2725,7 @@ static int xe_oa_init_gt(struct xe_gt *gt)
 
 	__xe_oa_init_oa_units(gt);
 
-	drmm_mutex_init(&gt_to_xe(gt)->drm, &gt->oa.gt_lock);
-
-	return 0;
+	return drmm_mutex_init(&gt_to_xe(gt)->drm, &gt->oa.gt_lock);
 }
 
 static void xe_oa_print_gt_oa_units(struct xe_gt *gt)
@@ -2867,7 +2865,10 @@ int xe_oa_init(struct xe_device *xe)
 	oa->xe = xe;
 	oa->oa_formats = oa_formats;
 
-	drmm_mutex_init(&oa->xe->drm, &oa->metrics_lock);
+	ret = drmm_mutex_init(&oa->xe->drm, &oa->metrics_lock);
+	if (ret)
+		goto exit;
+
 #ifdef __Linux__
 	idr_init_base(&oa->metrics_idr, 1);
 #elif defined(__FreeBSD__)
